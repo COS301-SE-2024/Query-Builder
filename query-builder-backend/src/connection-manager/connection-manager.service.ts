e33632a558954b4540993ca8a8360affafe834ba
Keanumrc <u20693398@tuks.co.za>
2024-06-01 14:39:20 +0200

    feat(database-connection): added mySQL driver

--- query-builder-backend/src/connection-manager/connection-manager.service.ts
+++ query-builder-backend/src/connection-manager/connection-manager.service.ts
@@ -1,10 +1,36 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+
+interface DatabaseCredentials {
+    host: string,
+    user: string,
+    password: string
+}
+
+export interface ConnectionStatus {
+    success: boolean,
+    connectionID?: number 
+}
 
 @Injectable()
-export class ConnectionManagerService implements OnModuleInit {
+export class ConnectionManagerService {
 
-    async onModuleInit() {
-        
-    }
+    connectToDatabase(credentials: DatabaseCredentials): Promise<ConnectionStatus> {
+        return new Promise((resolve, reject) => {
+          const connection = require('mysql').createConnection({
+            host: credentials.host,
+            user: credentials.user,
+            password: credentials.password,
+          });
+    
+          connection.connect((err) => {
+            if (err) {
+                console.log(err)
+                reject({ success: false }); // Reject with an error object
+            } else {
+                resolve({ success: true, connectionID: connection.threadID }); // Resolve with connection info
+            }
+          });
+        });
+      }
 
 }
