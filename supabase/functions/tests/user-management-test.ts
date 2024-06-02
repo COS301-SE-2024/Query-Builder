1cd4bbfd06e7a5998826d1d04a339bca63f14569
Graeme Blain <graemeharryblain@gmail.com>
2024-06-02 19:24:57 +0200

    Feature: finished backend for updating username and password
    The testing still needs to be done howwver

--- supabase/functions/tests/user-management-test.ts
+++ supabase/functions/tests/user-management-test.ts
@@ -13,8 +13,10 @@ import { delay } from "https://deno.land/x/delay@v0.2.0/mod.ts";
 import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
 
 // Set up the configuration for the Supabase client
-const supabase_url = Deno.env.get("SUPABASE_URL") ?? "";
-const supabase_anon_key = Deno.env.get("SUPABASE_ANON_KEY") ?? "";
+const env = await load();
+const supabase_url = env.SUPABASE_URL;
+const supabase_anon_key = env.SUPABASE_ANON_KEY;
+const supabase_service_role_key = env.SUPABASE_SERVICE_ROLE_KEY;
 const options = {
   auth: {
     autoRefreshToken: false,
@@ -29,8 +31,191 @@ const client: SupabaseClient = createClient(
   options,
 );
 
-const test1 = async () => {
-    assert(true);
+function logError(func_error: Error): Promise<void> {
+  console.log(func_error);
+  // const errorBodyReader = func_error.context.body.getReader();const { value, done } = await errorBodyReader.read();
+  // const errorBody = new TextDecoder().decode(value);
+  // console.log(errorBody);
+
+  throw new Error("Invalid response: " + func_error.message);
+}
+
+const test_no_body = async () => {
+  const { error } = await client.functions.invoke("user-management");
+  const error_message = error.context.headers.get("x-error-message");
+  assertEquals(error_message, "Unexpected end of JSON input");
+  await error.context.body.cancel();
 };
 
-Deno.test("Test 1", test1);
+const test_no_function_id = async () => {
+  const { error } = await client.functions.invoke("user-management", {
+    body: {},
+  });
+  const error_message = error.context.headers.get("x-error-message");
+  assertEquals(error_message, "Function ID is required");
+  await error.context.body.cancel();
+};
+
+const test_function_id_not_found = async () => {
+  const { error } = await client.functions.invoke("user-management", {
+    body: { function_id: "not-found" },
+  });
+  const error_message = error.context.headers.get("x-error-message");
+  assertEquals(error_message, "Function ID not found");
+  await error.context.body.cancel();
+};
+
+const test_update_username_no_username = async () => {
+  const { data: _data, error } = await client.functions.invoke("user-management", {
+    body: { function_id: "update-username" },
+  });
+  const error_message = error.context.headers.get("x-error-message");
+  assertEquals(error_message, "Username is required");
+  await error.context.body.cancel();
+}
+
+const test_update_username = async () => {
+  const client_super: SupabaseClient = createClient(
+    supabase_url,
+    supabase_service_role_key,
+    options,
+  );
+
+  const currentTime = new Date().toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }).replace(/:/g, "");
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const convertedTime = Array.from(currentTime).map((digit) =>
+    alphabet[parseInt(digit) - 1]
+  ).join("");
+
+  const email = "test" + convertedTime + "@example.com";
+  const password = "password";
+  const { data: _create_user, error: create_user_error } = await client_super.auth
+    .admin
+    .createUser({
+      email: email,
+      password: password,
+      email_confirm: true,
+    });
+
+  if (create_user_error) {
+    await logError(create_user_error);
+  }
+
+  const { data: test_data, error: sign_in_error } = await client.auth
+    .signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+  if (sign_in_error) {
+    await logError(sign_in_error);
+  }
+  const _supabase_user_jwt = test_data.session?.access_token;
+
+  const {data: update_data, error: update_error } = await client.functions.invoke("user-management", {
+    body: { function_id: "update-username", username: "new-username" },
+  });
+
+  if (update_error) {
+    await logError(update_error);
+  }
+  assertEquals(update_data.data.username, "new-username");
+
+  // delete the user
+  const { error: delete_error } = await client_super.auth
+    .admin
+    .deleteUser(test_data.user?.id!);
+
+  if (delete_error) {
+    await logError(delete_error);
+  }
+};
+
+const test_update_password_no_password = async () => {
+  const { data: _data, error } = await client.functions.invoke("user-management", {
+    body: { function_id: "update-password" },
+  });
+  const error_message = error.context.headers.get("x-error-message");
+  assertEquals(error_message, "Password is required");
+  await error.context.body.cancel();
+}
+
+const test_update_password = async () => {
+  const client_super: SupabaseClient = createClient(
+    supabase_url,
+    supabase_service_role_key,
+    options,
+  );
+
+  const currentTime = new Date().toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }).replace(/:/g, "");
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const convertedTime = Array.from(currentTime).map((digit) =>
+    alphabet[parseInt(digit) - 1]
+  ).join("");
+
+  const email = "test" + convertedTime + "@example.com";
+  const password = "password";
+  const { data: _create_user, error: create_user_error } = await client_super.auth
+    .admin
+    .createUser({
+      email: email,
+      password: password,
+      email_confirm: true,
+    });
+
+  if (create_user_error) {
+    await logError(create_user_error);
+  }
+
+  const { data: test_data, error: sign_in_error } = await client.auth
+    .signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+  if (sign_in_error) {
+    await logError(sign_in_error);
+  }
+  const _supabase_user_jwt = test_data.session?.access_token;
+
+  const {data: update_data, error: update_error } = await client.functions.invoke("user-management", {
+    body: { function_id: "update-password", password: "new-password" },
+  });
+
+  if (update_error) {
+    await logError(update_error);
+  }
+
+  console.log(update_data);
+  assertEquals(update_data.data.user.email, email);
+
+  // delete the user
+  const { error: delete_error } = await client_super.auth
+    .admin
+    .deleteUser(test_data.user?.id!);
+
+  if (delete_error) {
+    await logError(delete_error);
+  }
+
+}
+
+try {
+  Deno.test("Test no function body", test_no_body);
+  Deno.test("Test no function id", test_no_function_id);
+  Deno.test("Test function id not found", test_function_id_not_found);
+  Deno.test("Test update-username no username", test_update_username_no_username);
+  Deno.test("Test update-username", test_update_username);
+  Deno.test("Test update-password no password", test_update_password_no_password);
+  Deno.test("Test update-password", test_update_password);
+} catch (e) {
+  console.error(e);
+}
--- supabase/functions/user-management/index.ts
+++ supabase/functions/user-management/index.ts
@@ -13,77 +13,179 @@ import {
 const corse_headers = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers":
-    "authorization, x-client-info, apikey, content-type",
+    "authorization, x-client-info, apikey, content-type, x-error-message",
   "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE",
 };
 
-console.log("Hello from Functions!");
+function return_error(message: string, status: number) {
+  return new Response(JSON.stringify({ error: message }), {
+    headers: {
+      ...corse_headers,
+      "Content-Type": "application/json",
+      "x-error-message": message,
+    },
+    status,
+  });
+}
+
+function return_success(data: JSON) {
+  return new Response(
+    JSON.stringify({
+      data: data,
+    }),
+    {
+      headers: { ...corse_headers, "Content-Type": "application/json" },
+      status: 200,
+    },
+  );
+}
 
 Deno.serve(async (req: Request) => {
-  const { url, method } = req;
+  const { method } = req;
 
   if (method === "OPTIONS") {
     return new Response("ok", { headers: corse_headers });
   } else if (method === "POST" || method === "PUT") {
     try {
-      const authHeader = req.headers.get("Authorization")!;
+      const auth_header = req.headers.get("Authorization")!;
+      if (!auth_header) {
+        return await return_error("Authorization header is required", 400);
+      }
 
       const supabase_client: SupabaseClient = createClient(
         Deno.env.get("SUPABASE_URL") ?? "",
         Deno.env.get("SUPABASE_ANON_KEY") ?? "",
         {
           global: {
-            headers: { Authorization: authHeader },
+            headers: { Authorization: auth_header },
           },
         },
       );
 
-      const taskPath = new URLPattern({ pathname: "/user-management/:id" });
-      const matchingPath = taskPath.exec(url);
-      const function_id = matchingPath
-        ? matchingPath.pathname.groups.function_id
-        : null;
-      const body = await req.json();
+      const token = auth_header.replace("Bearer ", "");
 
-      if (function_id === null || body === null) {
-        throw new Error("Invalid request");
+      const json = await req.text();
+      if (!json) {
+        return await return_error("Unexpected end of JSON input", 400);
       }
 
-      // switch(function_id) {
-      //   // TODO - Add various sub-functions here
-      // }
+      const body = JSON.parse(json);
 
-      return new Response(
-        JSON.stringify({
-          message: "Hello from Functions!",
-        }),
-        {
-          headers: { ...corse_headers, "Content-Type": "application/json" },
-          status: 200,
-        },
-      );
+      if (!("function_id" in body)) {
+        return await return_error("Function ID is required", 400);
+      }
+
+      switch (body.function_id) {
+        case "update-username":
+          return update_username(supabase_client, body, token);
+        case "update-password":
+          return update_password(supabase_client, body, token);
+        default:
+          return await return_error("Function ID not found", 400);
+      }
     } catch (error) {
       console.error(error);
-      return new Response(
-        JSON.stringify({
-          error: error.message,
-        }),
-        {
-          headers: { ...corse_headers, "Content-Type": "application/json" },
-          status: 400,
-        },
-      );
+      return await return_error(error.event_message, 500);
     }
   }
 
   return new Response(
     JSON.stringify({
-      error: "Invalid request",
+      data: "Hello, IDK how you got here tbh",
     }),
-    { status: 400 },
+    {
+      headers: { ...corse_headers, "Content-Type": "application/json" },
+      status: 418,
+    },
   );
 });
 
+async function update_username(
+  supabase_client: SupabaseClient,
+  body: JSON,
+  token: string,
+) {
+  if (!("username" in body)) {
+    return await return_error("Username is required", 400);
+  }
+
+  const { data: req_data, error: req_error } = await supabase_client.auth
+    .getUser(token);
+  if (req_error) {
+    return await return_error(req_error.message, 500);
+  }
+
+  if (!req_data.user) {
+    return await return_error("User not found", 404);
+  }
+  const user = req_data.user;
+
+  const { username } = body;
+  const { error: update_error } = await supabase_client
+    .from("users")
+    .update({ username })
+    .eq("user_id", user.id);
+
+  if (update_error) {
+    return await return_error(update_error.message, 500);
+  }
+
+  const { data: updated_user, error: updated_user_error } =
+    await supabase_client
+      .from("users")
+      .select("*")
+      .eq("user_id", user.id)
+      .single();
+
+  if (updated_user_error) {
+    return await return_error(updated_user_error.message, 500);
+  }
+
+  return await return_success(updated_user);
+}
+
+async function update_password(
+  supabase_client: SupabaseClient,
+  body: JSON,
+  token: string,
+) {
+  if (!("password" in body)) {
+    return await return_error("Password is required", 400);
+  }
+
+  const { data: req_data, error: req_error } = await supabase_client.auth
+    .getUser(token);
+
+  if (req_error) {
+    return await return_error(req_error.message, 500);
+  }
+
+  const user = req_data.user;
+
+  const { password } = body;
+
+  const supabase_client_admin: SupabaseClient = createClient(
+    Deno.env.get("SUPABASE_URL") ?? "",
+    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "",
+    {
+      global: {
+        headers: { Authorization: `Bearer ${Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")}` },
+      },
+    },
+  );
+
+  const { data: updated_user, error } = await supabase_client_admin.auth.admin
+    .updateUserById(
+      user.id,
+      { password: password as string },
+    );
+
+  if (error) {
+    return await return_error(error.message, 500);
+  }
+
+  return await return_success(JSON.parse(JSON.stringify(updated_user)))
+}
 /* To invoke locally:
 
   1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
