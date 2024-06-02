db333332a3fe1b2b1d08e5b07267994487a7e05a
XadrianvHeerden <xadrian.vanheerden@tuks.co.za>
2024-06-02 03:04:00 +0200

    basic form design done

--- query-builder-app/src/components/Form/Form.tsx
+++ query-builder-app/src/components/Form/Form.tsx
@@ -1,104 +1,193 @@
 "use client"
 import "../../app/globals.css"
 import React, { useState } from "react";
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input, Spacer} from "@nextui-org/react";
+import {Button, Input, Spacer, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Card, CardHeader, CardBody, CardFooter} from "@nextui-org/react";
 
-export default function DatabaseConnectionModal(){
 
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+export default function Form(){
 
-    const [databaseConnectionStatus, setDatabaseConnectionStatus] = useState('Not connected to a database');
+    // const {isOpen, onOpen, onOpenChange} = useDisclosure();
 
-    const [url, setUrl] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [selectedDatabase, setSelectedDatabase] = useState(new Set(["Select database"]));
+    const selectedDatabaseValue = React.useMemo(
+        () => Array.from(selectedDatabase).join(", ").replaceAll("_", " "),
+        [selectedDatabase]
+    );
 
-    const connectToDatabase = (host:string, user:string, password:string) => {
+    const [selectedTable, setSelectedTable] = useState(new Set(["Select table"]));
+    const selectedTableValue = React.useMemo(
+        () => Array.from(selectedTable).join(", ").replaceAll("_", " "),
+        [selectedTable]
+    );
 
-      fetch("http://localhost:55555/api/connect", {
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+    const [selectColumns, setSelectedColumns] = React.useState(new Set());
+    const selectedColValue = React.useMemo(
+        () => Array.from(selectColumns).join(", ").replaceAll("_", " "),
+        [selectColumns]
+    );
+
+    const handleTableSelection = (keys:any) => {
+        if (keys.size === 0) {
+            setSelectedTable(new Set(["Select table"])); // Reset to default
+        } else {
+            setSelectedTable(keys);
+        }
+        setSelectedColumns(new Set()); // Clear selected columns
+    };
+
+    const handleColumnSelection = (keys:any) => {
+        if (keys.has("Select All")) {
+            if (selectColumns.size === tables.find(t => t.table === selectedTableValue)?.columns.length) {
+                setSelectedColumns(new Set());
+            } else {
+                const allColumns = tables.find(t => t.table === selectedTableValue)?.columns || [];
+                setSelectedColumns(new Set(allColumns));
+            }
+        } else {
+            setSelectedColumns(keys);
+        }
+    };
+
+    const databases = [
+        {
+          key: "My_Company_database",
+          label: "My Company Database",
+        }
+    ];
+
+    const tables = [
+        {
+            table: "Users",
+            columns: ["Name","Surname","Email"],
         },
-        body: JSON.stringify({
-          host: host,
-          user: user,
-          password: password
-        })
-      }).then(
-        function(response){
-          if(response.ok){
-            return response.json();
-          }
-          else{
-            return ({"success" : false})
-          }
+        {
+            table: "Finance",
+            columns: ["Month","Total","Average"],
         }
-      ).then(
-        function(response){
-          console.log(response)
-          if(response.success == true){
-            setDatabaseConnectionStatus("Connected to database")
-          }
-          else{
-            setDatabaseConnectionStatus("Not connected to a database")
-          }
-        }
-      )
+    ];
 
+    const sendQuery = (query:string) => {
+        
     }
 
     return (
 
         <>
-        <h1>{databaseConnectionStatus}</h1>
-        <Spacer y={5}/>
-        <Button onPress={onOpen} color="primary">Connect to a database</Button>
-        <Modal 
-          isOpen={isOpen} 
-          onOpenChange={onOpenChange}
-          placement="top-center"
-        >
-          <ModalContent>
-            {(onClose : any) => (
-              <>
-                <ModalHeader className="flex flex-col gap-1">Connect to a database</ModalHeader>
-                <ModalBody>
-                  <Input
-                    autoFocus
-                    label="URL"
-                    placeholder="Enter the database URL"
-                    variant="bordered"
-                    onValueChange={setUrl}
-                  />
-                  <Input
-                    autoFocus
-                    label="Username"
-                    placeholder="Enter your username"
-                    variant="bordered"
-                    onValueChange={setUsername}
-                  />
-                  <Input
-                    label="Password"
-                    placeholder="Enter your password"
-                    type="password"
-                    variant="bordered"
-                    onValueChange={setPassword}
-                  />
-                </ModalBody>
-                <ModalFooter>
-                  <Button color="danger" variant="flat" onPress={onClose}>
-                    Close
-                  </Button>
-                  <Button color="primary" onPress={onClose}  onClick={() => connectToDatabase(url, username, password)}>
-                    Connect
-                  </Button>
-                </ModalFooter>
-              </>
-            )}
-          </ModalContent>
-        </Modal>
+        <Card>
+            <CardHeader>
+                <div className="flex">
+                    <h1>Create a query</h1>
+                </div>
+            </CardHeader>
+            <CardBody> 
+                {/* add databases */}
+                <div className="flex">
+                    <h2>Database to query:</h2>
+                </div>
+                <Dropdown className="text-black">
+                    <DropdownTrigger>
+                        <Button 
+                        variant="bordered" 
+                        className="capitalize"
+                        >
+                        {selectedDatabaseValue}
+                        </Button>
+                    </DropdownTrigger>
+                    <DropdownMenu 
+                    aria-label="Dynamic Actions" 
+                        items={databases}
+                        variant="flat"
+                        disallowEmptySelection
+                        selectionMode="single"
+                        selectedKeys={selectedDatabase}
+                        onSelectionChange={setSelectedDatabase}
+                    >
+                        {(item) => (
+                        <DropdownItem
+                            key={item.key}
+                        >
+                            {item.label}
+                        </DropdownItem>
+                        )}
+                    </DropdownMenu>
+                </Dropdown>
+                <Spacer y={2}/>
+                {/* Select columns */}
+                <div className="flex">
+                    <h2>Table to query:</h2>
+                </div>
+                <Dropdown className="text-black">
+                    <DropdownTrigger>
+                        <Button 
+                        variant="bordered" 
+                        className="capitalize"
+                        >
+                        {selectedTableValue}
+                        </Button>
+                    </DropdownTrigger>
+                    <DropdownMenu 
+                        aria-label="Dynamic Actions" 
+                        items={tables} 
+                        variant="flat"
+                        selectionMode="single"
+                        selectedKeys={selectedTable}
+                        onSelectionChange={handleTableSelection}
+                    >
+                        {(item) => (
+                        <DropdownItem
+                            key={item.table}
+                        >
+                            {item.table}
+                        </DropdownItem>
+                        )}
+                    </DropdownMenu>
+                </Dropdown>
+                <Spacer y={2}/>
+                {/* select columns */}
+                
+                {!selectedTable.has("Select table") ? 
+                    (<>
+                        <div className="flex">
+                            <h2>Select the columns to display:</h2>
+                        </div>
+                        <Dropdown className="text-black">
+                            <DropdownTrigger>
+                                <Button 
+                                variant="bordered" 
+                                className="capitalize"
+                                >
+                                {selectedColValue}
+                                </Button>
+                            </DropdownTrigger>
+                            <DropdownMenu 
+                                aria-label="Multiple selection example"
+                                variant="flat"
+                                items={tables} 
+                                closeOnSelect={false}
+                                // disallowEmptySelection
+                                selectionMode="multiple"
+                                selectedKeys={selectColumns}
+                                onSelectionChange={handleColumnSelection}
+                            >
+                                <DropdownItem key="Select All">
+                                    Select All
+                                </DropdownItem>
+                                {tables.map((item) => (
+                                    (item.table == selectedTableValue)?(
+                                        (item.columns).map((col) =>
+                                            <DropdownItem
+                                            key={col}
+                                        >
+                                            {col}
+                                        </DropdownItem>) 
+                                    ):(null)
+                                ))}
+                            </DropdownMenu>
+                        </Dropdown>
+                    </>):
+                    (<></>)}
+            </CardBody>
+        </Card>
       </>
     )
 
