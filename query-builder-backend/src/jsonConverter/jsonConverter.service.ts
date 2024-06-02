e4e58b275279a1f8fc81091faa950ea0a9b92fd5
Zane Augustyn <u22528424@tuks.co.za>
2024-06-02 21:32:49 +0200

    feature/jsonConverter: fixed WHERE bug

--- query-builder-backend/src/jsonConverter/jsonConverter.service.ts
+++ query-builder-backend/src/jsonConverter/jsonConverter.service.ts
@@ -7,23 +7,29 @@ export class JsonConverterService {
         let query = '';
         jsonData.language = jsonData.language.toLowerCase();
         jsonData.query_type = jsonData.query_type.toLowerCase();
+    
         if (jsonData.language === 'sql') {
             if (jsonData.query_type === 'select') {
-                if (jsonData.table === '' || jsonData.column === '') {
-                    return 'Invalid query';
+                if (!jsonData.table || !jsonData.column) {
+                    throw new Error('Invalid query');
                 }
+                
                 const select = jsonData.column;
                 const from = jsonData.table;
-                const where = jsonData.condition;
-        
-                query = `SELECT ${select} FROM ${from} WHERE ${where}`;
+                let where = '';
+
+                if (jsonData.condition) {
+                    where = ` WHERE ${jsonData.condition}`;
+                }
+                
+                query = `SELECT ${select} FROM ${from}${where}`;
             } else {
-                query = 'Unsupported query type';
+                throw new Error('Unsupported query type');
             }
         } else {
             throw new Error('Invalid language');
         }
-
+    
         return query;
     }
 }
