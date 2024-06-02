9c0763a57dc421a085c978f05697506c33a870f0
Aidan Chapman (u22738917) <u22738917@tuks.co.za>
2024-06-02 12:39:52 +0200

    feat(devops): added commands, eslint, prettier and vitest

---
+++ query-builder-app/eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+
+export default [
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+];
