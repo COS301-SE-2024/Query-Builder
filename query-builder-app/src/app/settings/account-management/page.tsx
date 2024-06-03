0926b9cb345b3b95ded12d964b8e4a0d7079c275
Aidan Chapman (u22738917) <u22738917@tuks.co.za>
2024-06-03 10:47:50 +0200

    fix: still removing non-production files

--- query-builder-app/src/app/settings/account-management/page.tsx
+++
@@ -1,9 +0,0 @@
-'use-client'
-import { AccountManagement } from '../../../components/AccountManagement/AccountManagement'
-import styles from './page.module.css';
-
-export interface AccountManagementProps {}
-
-export default function AccountManagementPage(props: AccountManagementProps) {
-  return <AccountManagement />;
-}
