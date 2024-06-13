f231149f8b0353f383ec4c13c8f61c813aae2b92
Aidan Chapman <u22738917@tuks.co.za>
2024-06-13 11:02:43 +0200

    feat: started work on landing page

--- query-builder-app/src/app/page.tsx
+++ query-builder-app/src/app/page.tsx
@@ -8,6 +8,7 @@ export default function Page() {
   const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
   // todo : 1. get userdata from supabase using getUser()
+  
   // todo : 2. conditional rendering based on return from supabase
   if()
 	{
