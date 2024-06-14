efc4729faa80b41632cb8bcd90b5926c9d9a1376
Aidan Chapman <u22738917@tuks.co.za>
2024-06-14 23:26:22 +0200

    feat: fixed issues with the landing page conditional rendering

--- query-builder-app/src/app/page.tsx
+++ query-builder-app/src/app/page.tsx
@@ -1,21 +1,39 @@
+'use client';
 import LandingPage from '@/components/LandingPage/LandingPage';
-import { createClient } from '@supabase/supabase-js';
+import SignedInHomePage from '@/components/SignedInHomePage/SignedInHomePage';
+import { createClient, SupabaseClient, User } from '@supabase/supabase-js';
+import { useState, useEffect } from 'react';
 
 export default function Page() {
-  const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
-  const supabaseAnonKey: string =
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
-  const supabase = createClient(supabaseUrl, supabaseAnonKey);
+  //Connection to supabase
+  const supabaseURL: string = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
+  const supabaseANON: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
+  const supabase = createClient(supabaseURL, supabaseANON);
 
-  // todo : 1. get userdata from supabase using getUser()
+  // Setting up the user hook
+  const [user, setUser] = useState<User | null>(null);
+
+  // getUserdata is an asynchronous function used to query supabase for client data
+  const getUserData = async () => {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    setUser(user);
+  };
+
+  // Will trigger if the user is updated - idk if this is necessary - need to check with Xadrian
+  /*
+  useEffect(() => {
+
+  }, []);
+  */
   
-  // todo : 2. conditional rendering based on return from supabase
-  if()
-	{
-		return <LandingPage></LandingPage>;
-	}
-	else
-	{
-		// return <></> Keanu's stuff
-	}
+  getUserData();
+
+  // Determining which page to render based on whether the user is logged in or not
+  if (user === null) {
+    return <LandingPage />;
+  } else {
+    return <SignedInHomePage />;
+  }
 }
