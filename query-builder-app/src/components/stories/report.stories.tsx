455d7bd6327928009125da4a17d75a82ae7dd468
Zane Augustyn <u22528424@tuks.co.za>
2024-06-16 21:41:53 +0200

    feat: Add Report component for generating PDF reports

---
+++ query-builder-app/src/components/stories/report.stories.tsx
@@ -0,0 +1,14 @@
+// import the component you want to make a story for
+import React from "react";
+import "../../app/globals.css";
+import Report from "../report/report";
+
+// Setting up the story's default component
+export default {
+    component: Report
+}
+
+// Can have multiple different variants of a component
+export const DefaultReport = {
+    render: () => <Report/>
+};
