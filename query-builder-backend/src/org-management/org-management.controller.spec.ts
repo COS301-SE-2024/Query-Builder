0d1f68b7488d930453e6ecb630da3f4fe55bef9d
Graeme Blain <graemeharryblain@gmail.com>
2024-06-17 12:47:29 +0200

    fixes to testing

--- query-builder-backend/src/org-management/org-management.controller.spec.ts
+++ query-builder-backend/src/org-management/org-management.controller.spec.ts
@@ -1,13 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrgManagementController } from './org-management.controller';
 import { Supabase } from '../supabase';
+import { ConfigService } from '@nestjs/config';
 
 describe('OrgManagementController', () => {
   let controller: OrgManagementController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [OrgManagementController],
+      controllers: [OrgManagementController, Supabase, ConfigService],
     }).compile();
 
     controller = module.get<OrgManagementController>(OrgManagementController);
--- query-builder-backend/src/org-management/org-management.service.spec.ts
+++ query-builder-backend/src/org-management/org-management.service.spec.ts
@@ -1,13 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrgManagementService } from './org-management.service';
 import { Supabase } from '../supabase';
+import { ConfigService } from '@nestjs/config';
 
 describe('OrgManagementService', () => {
   let service: OrgManagementService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrgManagementService],
+      providers: [OrgManagementService, Supabase, ConfigService],
     }).compile();
 
     service = module.get<OrgManagementService>(OrgManagementService);
