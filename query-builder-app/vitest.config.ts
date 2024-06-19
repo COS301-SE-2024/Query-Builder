2b092a0ff995374178769f3e5d8c521f29fe4c1e
Keanumrc <u20693398@tuks.co.za>
2024-06-19 21:34:41 +0200

    feat(actions): added lcov output format for coverage

--- query-builder-app/vitest.config.ts
+++ query-builder-app/vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     coverage: {
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text', 'json', 'html', 'lcov'],
     },
   },
 })
