399041785bf802bf6c199a7802195a7ed2f0982b
Graeme Blain <graemeharryblain@gmail.com>
2024-07-07 00:00:37 +0200

    Added exceotion filter and updated user-management

--- query-builder-backend/src/main.ts
+++ query-builder-backend/src/main.ts
@@ -1,9 +1,13 @@
-import { NestFactory } from "@nestjs/core";
+import { NestFactory, HttpAdapterHost } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { AllExceptionsFilter } from "./all-exceptions.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const { httpAdapter } = app.get(HttpAdapterHost);
   app.setGlobalPrefix("api");
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
   app.enableCors();
   await app.listen(55555);
--- query-builder-backend/src/user-management/user-management.service.ts
+++ query-builder-backend/src/user-management/user-management.service.ts
@@ -3,60 +3,75 @@ import {
   Injectable,
   InternalServerErrorException,
   NotFoundException,
-} from "@nestjs/common";
-import { Get_User_Dto } from "./dto/get-user.dto";
-import { Supabase } from "../supabase";
-import { Create_User_Dto } from "./dto/create-user.dto";
-import { Sign_In_User_Dto } from "./dto/sign-in-user.dto";
-import { Update_User_Dto } from "./dto/update-user.dto";
+} from '@nestjs/common';
+import { Get_User_Dto } from './dto/get-user.dto';
+import { Supabase } from '../supabase';
+import { Create_User_Dto } from './dto/create-user.dto';
+import { Sign_In_User_Dto } from './dto/sign-in-user.dto';
+import { Update_User_Dto } from './dto/update-user.dto';
+import { AuthApiError, PostgrestError } from '@supabase/supabase-js';
 
 @Injectable()
 export class UserManagementService {
   constructor(private supabase: Supabase) {}
 
+  // TODO: Test this function
   async getUser(user: Get_User_Dto) {
-    const { data, error } = await this.supabase.getClient()
-      .from("profiles")
+    const { data, error } = await this.supabase
+      .getClient()
+      .from('profiles')
       .select()
       .match({ ...user });
 
     if (error) {
-      throw new NotFoundException(error.message);
+      throw error;
+    }
+
+    if (data.length === 0) {
+      throw new NotFoundException('User not found');
     }
 
     return { data };
   }
 
+  // TODO: Test this function
   async getLoggedInUser() {
-    const { data, error } = await this.supabase.getClient().auth.getUser(this.supabase.getJwt());
+    const { data, error } = await this.supabase
+      .getClient()
+      .auth.getUser(this.supabase.getJwt());
 
     if (error) {
-      throw new NotFoundException(error.message);
+      throw error;
     }
 
     const user_id = data.user.id;
 
-    const { data: profile_data, error: profile_error } = await this.supabase.getClient()
-      .from("profiles")
+    const { data: profile_data, error: profile_error } = await this.supabase
+      .getClient()
+      .from('profiles')
       .select()
-      .eq("user_id", user_id);
+      .eq('user_id', user_id);
 
     if (profile_error) {
-      throw new NotFoundException(profile_error.message);
+      throw profile_error;
+    }
+    if (profile_data.length === 0) {
+      throw new NotFoundException('User not found');
     }
 
     return { profile_data };
   }
 
   async signIn(user: Sign_In_User_Dto) {
-    const { data, error } = await this.supabase.getClient().auth
-      .signInWithPassword({
+    const { data, error } = await this.supabase
+      .getClient()
+      .auth.signInWithPassword({
         email: user.email,
         password: user.password,
       });
 
     if (error) {
-      throw new HttpException(error.message, error.status);
+      throw error;
     }
 
     return { data };
@@ -75,15 +90,16 @@ export class UserManagementService {
     });
 
     if (error) {
-      throw new InternalServerErrorException(error.message);
+      throw error;
     }
 
     return { data };
   }
 
   async createUser(user: Create_User_Dto) {
-    const { data, error } = await this.supabase.getClient().auth.admin
-      .createUser({
+    const { data, error } = await this.supabase
+      .getClient()
+      .auth.admin.createUser({
         email: user.email,
         password: user.password,
         phone: user.phone,
@@ -95,44 +111,52 @@ export class UserManagementService {
       });
 
     if (error) {
-      throw new InternalServerErrorException(error.message);
+      throw error;
     }
 
     return { data };
   }
 
+  // TODO: Test this function
   async updateUser(user: Update_User_Dto) {
-    const { data: { user: user_data }, error: user_error } = await this.supabase
-      .getClient().auth.getUser(this.supabase.getJwt());
+    const {
+      data: { user: user_data },
+      error: user_error,
+    } = await this.supabase.getClient().auth.getUser(this.supabase.getJwt());
 
     if (user_error) {
-      throw new HttpException(user_error.message, user_error.status);
+      throw user_error;
     }
 
     const user_id = user_data.id;
 
-    const { data, error } = await this.supabase.getClient()
-      .from("profiles")
+    const { data, error } = await this.supabase
+      .getClient()
+      .from('profiles')
       .update({ ...user })
-      .eq("user_id", user_id)
+      .eq('user_id', user_id)
       .select();
 
     if (error) {
-      throw new InternalServerErrorException(error.message);
+      throw error;
+    }
+
+    if (data.length === 0) {
+      throw new NotFoundException('User not found');
     }
 
     return { data };
   }
 
   async updateUserPassword(user: Update_User_Dto) {
-    return { data: "Not implemented" };
+    return { data: 'Not implemented' };
   }
 
   async updateUserEmail(user: Update_User_Dto) {
-    return { data: "Not implemented" };
+    return { data: 'Not implemented' };
   }
 
   async updateUserPhone(user: Update_User_Dto) {
-    return { data: "Not implemented" };
+    return { data: 'Not implemented' };
   }
 }
