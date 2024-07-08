1a7664b4de5fbb5d9ad284733efcc552966217b5
XadrianvHeerden <xadrian.vanheerden@tuks.co.za>
2024-07-08 13:23:18 +0200

    added testing

--- query-builder-app/src/components/UserSettings/UserSettings.spec.tsx
+++ query-builder-app/src/components/UserSettings/UserSettings.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import {  describe, it, expect,vi } from 'vitest';
+import { describe, it, expect,vi } from 'vitest';
 import '@testing-library/jest-dom';
 import UserSettings from './UserSettings'; // adjust the import according to your file structure
 
