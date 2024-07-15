19032c6ee87b469b8b35b2cef752293b555b53f5
Graeme Blain <graemeharryblain@gmail.com>
2024-07-15 14:23:18 +0200

    Dto changes

--- query-builder-backend/src/org-management/dto/add-member.dto.ts
+++ query-builder-backend/src/org-management/dto/add-member.dto.ts
@@ -1,15 +1,23 @@
-import { IsEnum, IsNotEmpty, IsString, IsUUID } from "class-validator";
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
 
 export class Add_Member_Dto {
-    @IsUUID()
-    @IsNotEmpty()
-    org_id: string;
+  @IsUUID()
+  @IsNotEmpty()
+  org_id: string;
 
-    @IsUUID()
-    @IsNotEmpty()
-    user_id: string;
+  @IsUUID()
+  @IsNotEmpty()
+  user_id: string;
 
-    @IsEnum(["admin", "member"])
-    @IsNotEmpty()
-    user_role: string;
+  @IsEnum(['admin', 'member'], {
+    message: "user_role must be either 'admin' or 'member'",
+  })
+  @IsNotEmpty()
+  user_role: string;
 }
