80493db88602cf48675c7edc97ec030aedbfed39
Zane Augustyn <u22528424@tuks.co.za>
2024-07-29 09:04:40 +0200

    chore(QueryManagementService): add getQueries method

--- query-builder-backend/src/query-management/query-management.service.ts
+++ query-builder-backend/src/query-management/query-management.service.ts
@@ -22,7 +22,8 @@ export class QueryManagementService {
         const saved_query_fields = {
             parameters: save_query_dto.parameters,
             user_id: user_data.user.id,
-            db_id: save_query_dto.db_id
+            db_id: save_query_dto.db_id,
+            queryTitle: save_query_dto.queryTitle
         }
 
         const { data: save_data, error: save_error } = await this.supabase
@@ -40,6 +41,34 @@ export class QueryManagementService {
 
         return { save_data };
 
+    }
+
+    async getQueries(){
+            
+            //Firstly get the user who is saving the query
+            const { data: user_data, error: user_error } = await this.supabase
+            .getClient()
+            .auth.getUser(this.supabase.getJwt());
+    
+            if(user_error){
+                throw user_error;
+            }
+    
+            //Secondly get the queries saved by the user
+            const { data: query_data, error: query_error } = await this.supabase
+                .getClient()
+                .from('saved_queries')
+                .select()
+                .eq('user_id', user_data.user.id);
+    
+            if (query_error) {
+                throw query_error;
+            }
+            if (query_data.length === 0) {
+                throw new InternalServerErrorException('No queries found');
+            }
+    
+            return { query_data };
     }
 
 }
