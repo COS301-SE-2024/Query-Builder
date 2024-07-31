4397d450f9b70d492799fb603a9a7adc44d7cc43
Keanumrc <u20693398@tuks.co.za>
2024-07-31 16:29:24 +0200

    testing(TableForm): increased TableForm coverage

--- query-builder-app/src/components/TableForm/TableForm.spec.tsx
+++ query-builder-app/src/components/TableForm/TableForm.spec.tsx
@@ -4,7 +4,7 @@ import { render, screen } from '@testing-library/react';
 import { describe, it, expect, vitest, vi, Mock } from 'vitest';
 import TableForm from "./TableForm"
 import { table } from "@/interfaces/intermediateJSON";
-import { createClient } from '@/utils/supabase/client';
+import userEvent from '@testing-library/user-event'
 
 //Mock out Supabase access token retrieval
 vitest.mock("./../../utils/supabase/client", () => {
@@ -27,7 +27,7 @@ vitest.mock("./../../utils/supabase/client", () => {
 //Mock out the API call to retrieve the table's columns
 global.fetch = vi.fn(() =>
     Promise.resolve({
-      json: () => Promise.resolve({ data: [{name: "users"}, {name: "employees"}] }),
+      json: () => Promise.resolve({ data: [{name: "first_name"}, {name: "last_name"}] }),
     }),
   ) as Mock;
 
@@ -36,7 +36,7 @@ describe('TableForm basic rendering tests', () => {
     it('should render successfully', () => {
   
       const tableProp: table = {
-          name: "",
+          name: "users",
           columns: []
       }
   
@@ -44,4 +44,49 @@ describe('TableForm basic rendering tests', () => {
       expect(baseElement).toBeTruthy();
   
     });
-  });
+});
+
+describe('TableForm column selection tests', () => {
+    it('should be able to select a column and update a table', async () => {
+  
+        let tableProp: table = {
+            name: "users",
+            columns: []
+        }
+
+        //callback function for TableForm to modify table
+        function updateTable(table: table){
+
+            //modify tableProp
+            tableProp = table;
+
+        }
+
+        //create a user that can perform actions
+        const user = userEvent.setup();
+    
+        //render the TableForm
+        render(<TableForm table={tableProp} onChange={updateTable} />);
+
+        //get the add button
+        const button = screen.getAllByLabelText('addColumn')[0];
+
+        //click the add button
+        await user.click(button);
+
+        //find the users column
+        const userSelection = screen.getByLabelText("first_name");
+
+        //select the users column
+        await user.click(userSelection);
+
+        //check that tableProp now matches the expectedTable
+        const expectedTable: table = {
+            name: "users",
+            columns: [{name: "first_name"}]
+        }
+
+        expect(tableProp).toEqual(expectedTable);
+  
+    });
+});
