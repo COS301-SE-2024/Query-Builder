dac30494e8c0fdd027390292db9299b8f90ab97d
XadrianvHeerden <xadrian.vanheerden@tuks.co.za>
2024-08-03 13:27:02 +0200

    preparation for rebase

--- query-builder-app/src/components/OrganisationManagement/OrganisationManagement.tsx
+++ query-builder-app/src/components/OrganisationManagement/OrganisationManagement.tsx
@@ -120,9 +120,7 @@ export default function OrganisationManagement(){
         getOrganisationInfo();
       }, []);
 
-    // TODO: get members
-    
-   
+
     // // Updated fields
     const updateQuery = async() => {
 
