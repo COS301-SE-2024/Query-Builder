c15d4bc916ea8b2ba30aba77cca439040d440e52
Zane Augustyn <u22528424@tuks.co.za>
2024-08-05 09:37:29 +0200

    refactor: Update ContextMenuCard component to handle query deletion

--- query-builder-app/src/components/ContextMenuCard/ContextMenuCard.tsx
+++ query-builder-app/src/components/ContextMenuCard/ContextMenuCard.tsx
@@ -1,30 +1,81 @@
-import React from "react";
-import { Card, CardBody, CardFooter, Divider, CardHeader, Button, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, DropdownSection } from "@nextui-org/react";
+import React, { useState } from "react";
+import {
+    Button,
+    Dropdown,
+    DropdownTrigger,
+    DropdownMenu,
+    DropdownItem,
+    DropdownSection,
+} from "@nextui-org/react";
+import { createClient } from "./../../utils/supabase/client";
 
-export interface ContextMenuCardProps {
-    queryTitle: string,
-    saved_at: string,
-    parameters: any
+interface ContextMenuCardProps {
+    queryTitle: string;
+    saved_at: string;
+    parameters: any;
+    query_id: any;
+    db_id: string;
 }
 
-export default function ContextMenuCard({ queryTitle, saved_at, parameters }: ContextMenuCardProps) {
+// This function gets the token from local storage.
+// Supabase stores the token in local storage so we can access it from there.
+const getToken = async () => {
+    const supabase = createClient();
+    const token = (await supabase.auth.getSession()).data.session?.access_token;
+
+    console.log(token);
+
+    return token;
+};
+
+async function removeQuery(query_id: string) {
+    let response = await fetch("http://localhost:55555/api/query-management/delete-query", {
+        credentials: "include",
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + await getToken()
+        },
+        body: JSON.stringify({
+            query_id: query_id
+        })
+    });
+
+    if (!response.ok) {
+        // Handle errors or unsuccessful response
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    let json = (await response.json()).data;
+}
+
+export default function ContextMenuCard({
+    queryTitle,
+    saved_at,
+    parameters,
+    query_id,
+    db_id,
+}: ContextMenuCardProps) {
+    const [loading, setLoading] = useState(false);
+
+    const handleDelete = async () => {
+        setLoading(true);
+
+        try {
+            await removeQuery(query_id);
+        } catch (error) {
+            console.error("Error during deletion:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleRetrieve = async () => {
+        let query = parameters;
+    };
+
     return (
-
-        <Dropdown>
-            <DropdownTrigger>
-                <Button
-                    variant="flat" className="w-[235px]" color="primary"
-                >
-                    {queryTitle}
-                </Button>
-            </DropdownTrigger>
-            <DropdownMenu aria-label="Static Actions">
-                <DropdownSection title={saved_at}>
-                    <DropdownItem key="retrieve" description="Retrieve saved query">Retrieve Query</DropdownItem>
-                    <DropdownItem key="delete" className="text-danger" color="danger" description="Delete query from saved queries">Delete Query</DropdownItem>
-                </DropdownSection>
-
-            </DropdownMenu>
-        </Dropdown>
+        
     );
 }
