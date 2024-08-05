6291f5a4d875e6d88af5578afa50d6e4f7c4a9c6
Zane Augustyn <u22528424@tuks.co.za>
2024-08-05 09:38:11 +0200

    refactor: Update ContextMenu component to fetch and display saved queries

--- query-builder-app/src/components/ContextMenu/ContextMenu.tsx
+++ query-builder-app/src/components/ContextMenu/ContextMenu.tsx
@@ -1,81 +1,68 @@
 import React from "react";
-import { ScrollShadow, Divider } from "@nextui-org/react";
+import { ScrollShadow, Spacer } from "@nextui-org/react";
 import ContextMenuCard from "../ContextMenuCard/ContextMenuCard";
 import { createClient } from "./../../utils/supabase/client";
 
 export default function ContextMenu() {
 
 
-  interface ContextMenuCardProps {
-    queryTitle: string,
-    saved_at: string,
-    parameters: any
-  }
-
-  // This function gets the token from local storage.
-  // Supabase stores the token in local storage so we can access it from there.
-  const getToken = async () => {
-
-    const supabase = createClient();
-    const token = (await supabase.auth.getSession()).data.session?.access_token
-
-    console.log(token)
-
-    return token;
-  };
-
-  async function getSavedQueries() {
-    try {
-        const response = await fetch("http://localhost:55555/api/query-management/get-queries", {
-            credentials: "include",
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + await getToken()
-            }
-        });
-
-        console.log("Response-------------------", response);
-
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-
-        const data = await response.json();
-
-        console.log("Data-------------------", data)
-        setSavedQueries(data.query_data);
-    } catch (error) {
-        console.error("Failed to fetch saved queries:", error);
-        setSavedQueries([]);
+    interface ContextMenuCardProps {
+        queryTitle: string,
+        saved_at: string,
+        parameters: any,
+        query_id: any,
+        db_id: string,
     }
-}
+
+    // This function gets the token from local storage.
+    // Supabase stores the token in local storage so we can access it from there.
+    const getToken = async () => {
+
+        const supabase = createClient();
+        const token = (await supabase.auth.getSession()).data.session?.access_token
+
+        console.log(token)
+
+        return token;
+    };
+
+    async function getSavedQueries() {
+        try {
+            const response = await fetch("http://localhost:55555/api/query-management/get-queries", {
+                credentials: "include",
+                method: "GET",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + await getToken()
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+
+            const data = await response.json();
+
+            setSavedQueries(data.query_data);
+        } catch (error) {
+            console.error("Failed to fetch saved queries:", error);
+            setSavedQueries([]);
+        }
+    }
 
 
-  //React hook to hold the user's organisations
-  const [savedQueries, setSavedQueries] = React.useState<ContextMenuCardProps[]>([]);
+    //React hook to hold the user's organisations
+    const [savedQueries, setSavedQueries] = React.useState<ContextMenuCardProps[]>([]);
 
 
-  //React hook to fetch the user's organisations upon rerender of the component
-  React.useEffect(() => {
-    console.log("useEffect-------------------------------");
-    getSavedQueries();
+    //React hook to fetch the user's organisations upon rerender of the component
+    React.useEffect(() => {
+        getSavedQueries();
 
-  }, [])
+    }, [])
 
-return (
-    <ScrollShadow className=" h-[500px]">
-        {Array.isArray(savedQueries) && savedQueries.map((queryData: ContextMenuCardProps, index: number) => (
-            <React.Fragment key={index}>
-                <ContextMenuCard
-                    queryTitle={queryData.queryTitle}
-                    saved_at={queryData.saved_at}
-                    parameters={queryData.parameters}
-                />
-                <Divider className="my-1" key={`divider-${index}`} />
-            </React.Fragment>
-        ))}
-    </ScrollShadow>
-);
+    return (
+
+    );
 }
