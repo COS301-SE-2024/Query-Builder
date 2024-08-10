ffa29ef01486013b4ff43eb84e3207945739af71
Zane Augustyn <u22528424@tuks.co.za>
2024-08-10 18:12:49 +0200

    chore: Update NaturalLanguage component with NextUI Modal and TableResponse

--- query-builder-app/src/components/NaturalLanguage/NaturalLanguage.tsx
+++ query-builder-app/src/components/NaturalLanguage/NaturalLanguage.tsx
@@ -1,34 +1,60 @@
 import React from "react";
-import { Textarea, Button, ButtonGroup } from "@nextui-org/react";
+import { Textarea, Button, ButtonGroup, useDisclosure, Modal, ModalContent, ModalHeader, Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
+import TableResponse from "../TableResponse/TableResponse";
 
 export default function NaturalLanguage() {
     const [value, setValue] = React.useState("");
     const [loading, setLoading] = React.useState(false);
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const [query, setQuery] = React.useState("");
 
     const handleSubmit = () => {
         setLoading(true);
-
         //Here we will do chatGPT stuff
+        //give value to chatGPT
+
+
+
+        //get query from chatGPT
+        { onOpen }
     };
 
     return (
-        <div className="w-full h-[70%] flex flex-col gap-2 max-w-[80%] items-center">
-    <Textarea
-        className="w-[80%] h-[100%]"
-        label="Please enter desired data"
-        placeholder="Enter your description of desired data"
-        value={value}
-        onChange={(newVal) => setValue(newVal.target.value)}
-    />
-    <Button
-        isDisabled={value.length <= 1}
-        isLoading={loading}
-        onClick={handleSubmit}
-        color="primary"
-        className="w-[80%]"
-    >
-        Query
-    </Button>
-</div>
+        <Card>
+            <CardHeader>Type what you would like to query</CardHeader>
+            <CardBody>
+                <Textarea
+                    className="w-[100%] h-[100%] item-centered"
+                    placeholder="Type your query here"
+                    value={value}
+                    onChange={(newVal) => setValue(newVal.target.value)}
+                />
+            </CardBody>
+            <CardFooter>
+            <Button
+                isDisabled={value.length <= 1}
+                isLoading={loading}
+                color="primary"
+                onClick={handleSubmit}
+            >
+                Query
+            </Button>
+            <Modal
+                isOpen={isOpen}
+                onOpenChange={onOpenChange}
+                placement="top-center"
+                className="text-black h-100vh"
+                size="full">
+                <ModalContent>
+                    {(onClose: any) => (
+                        <>
+                            <ModalHeader className="flex flex-col gap-1">Query Results</ModalHeader>
+                            {/* <TableResponse query={query} /> */}
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+            </CardFooter>
+        </Card>
     );
 }
