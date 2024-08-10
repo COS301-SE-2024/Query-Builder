239b3e947793f7444aa38226fa7fef941f2d188d
Zane Augustyn <u22528424@tuks.co.za>
2024-08-10 18:13:12 +0200

    refactor: Update NaturalLanguage component with NextUI Modal and TableResponse

--- query-builder-app/src/app/[databaseServerID]/page.tsx
+++ query-builder-app/src/app/[databaseServerID]/page.tsx
@@ -1,49 +1,60 @@
 "use client"
 import Form from "@/components/Form/Form";
 import TableResponse from "@/components/TableResponse/TableResponse";
+import { Button, Image, Spacer, Card, CardBody, CardHeader, Input, Tabs, Tab, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, Tooltip, getKeyValue } from "@nextui-org/react";
 import { useState } from "react";
 import Sidebar from "../../components/SideBar/SideBar";
+import NaturalLanguage from "@/components/NaturalLanguage/NaturalLanguage";
 
 
 interface DatabaseCredentials {
-    host: string,
-    user: string,
-    password: string
-  }
-  
-  interface SortParams {
-    column: string,
-    direction?: "ascending"|"descending"
-  }
-  
-  interface PageParams {
-    pageNumber: number,
-    rowsPerPage: number
-  }
-  
-  interface QueryParams {
-    language: string,
-    query_type: string,
-    table: string,
-    columns: string[],
-    condition?: string,
-    sortParams?: SortParams,
-    pageParams?: PageParams
-  }
-  
-  interface Query {
+  host: string,
+  user: string,
+  password: string
+}
+
+interface SortParams {
+  column: string,
+  direction?: "ascending" | "descending"
+}
+
+interface PageParams {
+  pageNumber: number,
+  rowsPerPage: number
+}
+
+interface QueryParams {
+  language: string,
+  query_type: string,
+  table: string,
+  columns: string[],
+  condition?: string,
+  sortParams?: SortParams,
+  pageParams?: PageParams
+}
+
+interface Query {
   credentials: DatabaseCredentials,
   databaseName: string,
   queryParams: QueryParams
-  }
+}
 
 export default function QueryBuilder() {
-    const [query, setQuery] = useState<Query | undefined>(undefined);
+  const [query, setQuery] = useState<Query | undefined>(undefined);
 
-    return(
-      <div className="full-application">
-        <Sidebar/>
-        <Form/>
+  return (
+    <div className="full-application">
+      <Sidebar />
+      <div className="flex w-full flex-col center">
+        <Tabs>
+          <Tab key="form" title="QueryForm">
+            <Form />
+          </Tab>
+          <Tab key="naturalLanguage" title="QueryQuill">
+            <NaturalLanguage />
+          </Tab>
+        </Tabs>
       </div>
-    ) ;
+    </div>
+  );
 }
