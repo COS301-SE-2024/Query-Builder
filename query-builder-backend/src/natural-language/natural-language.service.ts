2ec3a4012d9461d41cb38d2b74e940b5b427616b
Keanumrc <u20693398@tuks.co.za>
2024-08-12 08:50:13 +0200

    feature(nlp): added support for OpenAI

--- query-builder-backend/src/natural-language/natural-language.service.ts
+++ query-builder-backend/src/natural-language/natural-language.service.ts
@@ -31,14 +31,129 @@ export class NaturalLanguageService {
     session: Record<string, any>
   ) {
     if (naturalLanguageQuery.llm === 'openAI') {
+
+      //-----------Fetch DB metadata to inform the LLM of the database server's structure-----------//
+      const metadataSummary =
+      await this.dbMetadataHandlerService.getSchemaSummary(
+        { databaseServerID: naturalLanguageQuery.databaseServerID },
+        session
+      );
+      const metadataSummaryString = JSON.stringify(metadataSummary);
+
+      //-------------------------------Create the prompt for the LLM-------------------------------//
+
+      let prompt: string =
+        'Given the following QueryParams interface, write a QueryParams object for the natural language query ' +
+        naturalLanguageQuery.query +
+        '\n\n';
+
+      prompt +=
+        'Return only the object not in a code block without any comments or other text. Language should be sql and query type should be select \n\n';
+
+      prompt += `export interface QueryParams {
+                            language: string,
+                            query_type: string,
+                            databaseName: string,
+                            table: table,
+                            condition?: condition,
+                            sortParams?: SortParams,
+                            pageParams?: PageParams
+                        }
+
+                        export interface table {
+                            name: string,
+                            columns: column[],
+                            join?: join
+                        }
+
+                        export interface column {
+                            name: string,
+                            aggregation? : AggregateFunction,
+                            alias?: string,
+                        }
+
+                        export interface join {
+                            table1MatchingColumnName: string,
+                            table2: table,
+                            table2MatchingColumnName: string,
+                        }
+
+                        export interface condition {
+                        }
+
+                        export interface compoundCondition extends condition{
+                            conditions: condition[],
+                            operator: LogicalOperator,
+                        }
+
+                        export interface primitiveCondition extends condition{
+                            value: string | number | boolean | null,
+                            tableName?: string,
+                            column: string,
+                            operator: ComparisonOperator,
+                            aggregate?: AggregateFunction
+                        }
+
+                        export interface SortParams {
+                            column: string,
+                            direction?: "ascending"|"descending"
+                        }
+
+                        export interface PageParams {
+                            //note pageNumbers are indexed from 1
+                            pageNumber: number,
+                            rowsPerPage: number
+                        }
+
+                        export enum AggregateFunction {
+                            COUNT = "COUNT",
+                            SUM = "SUM",
+                            AVG = "AVG",
+                            MIN = "MIN",
+                            MAX = "MAX",
+                        }
+
+                        export enum LogicalOperator {
+                            AND = "AND",
+                            OR = "OR",
+                            NOT = "NOT",
+                        }
+
+                        export enum ComparisonOperator {
+                            EQUAL = "=",
+                            LESS_THAN = "<",
+                            GREATER_THAN = ">",
+                            LESS_THAN_EQUAL = "<=",
+                            GREATER_THAN_EQUAL = ">=",
+                            NOT_EQUAL = "<>",
+                            LIKE = "LIKE",
+                            IS = "IS",
+                            IS_NOT = "IS NOT"
+                        }`;
+
+      prompt +=
+        '\n\n The database structure is as follows: ' + metadataSummaryString;
+
+      //--------------------Get the JSON intermediate form result from the LLM---------------------//
+
       const openAiResponse = this.openAiService.chat.completions.create({
-        model: 'gpt-4o-mini',
-        messages: [{ role: 'user', content: naturalLanguageQuery.query }]
+        model: 'gpt-4',
+        messages: [{ role: 'user', content: prompt }]
       });
 
-      return openAiResponse;
+      const textResponse = (await openAiResponse).choices[0].message.content;
+
+      const jsonResponse = JSON.parse(textResponse);
 
       //--------------------Send the JSON intermediate form to the QueryHandler--------------------//
+
+      const query: Query = {
+        databaseServerID: naturalLanguageQuery.databaseServerID,
+        queryParams: jsonResponse
+      };
+
+      return query;
+
     } else {
       //-----------Fetch DB metadata to inform the LLM of the database server's structure-----------//
       const metadataSummary =
