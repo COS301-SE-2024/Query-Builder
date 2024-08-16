10ca42c71b2dbd45efc19b2dc0ed6227373f3f6f
Keanumrc <u20693398@tuks.co.za>
2024-08-16 07:29:04 +0200

    testing(form): finally mocked out useParams successfully

--- query-builder-app/src/components/Form/Form.spec.tsx
+++ query-builder-app/src/components/Form/Form.spec.tsx
@@ -1,20 +1,36 @@
 import { render } from '@testing-library/react';
 import Form from './Form';
 import React from 'react';
-import { describe, it, expect } from 'vitest';
-import { MemoryRouter, Route } from 'react-router-dom';
+import { describe, it, expect, vi } from 'vitest';
+
+vi.mock('next/navigation', () => ({
+  useParams: () => ({ databaseServerID: 'mock-database-server-id' })
+}));
+
+//Mock out Supabase access token retrieval
+vi.mock("./../../utils/supabase/client", () => {
+  return{
+      createClient: vi.fn().mockImplementation(() => {
+          return{
+              auth: {
+              getSession: vi.fn().mockImplementation(() => {
+                  return{
+                      data: vi.fn().mockReturnThis(),
+                      session: vi.fn().mockReturnThis(),
+                      access_token: "randomAccessToken"
+                  }
+              })
+          }}
+      })
+  }
+})
 
 //basic component rendering tests
 describe('Form basic rendering tests', () => {
 
     it('should render successfully', () => {
 
-        const {baseElement} = render(
-        <MemoryRouter initialEntries={['/databaseServerID']}>
-          <Route path='/1234'>
-            <Form />
-          </Route>
-        </MemoryRouter>);
+        const {baseElement} = render(<Form/>);
         expect(baseElement).toBeTruthy();
 
     });
