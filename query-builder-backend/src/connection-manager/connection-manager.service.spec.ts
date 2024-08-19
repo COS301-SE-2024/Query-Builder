0250ff635c2c40af7304bf8850883321c811f9f1
Graeme Blain <graemeharryblain@gmail.com>
2024-08-19 09:39:20 +0200

    Mock supabase and sessions for connection manager

--- query-builder-backend/src/connection-manager/connection-manager.service.spec.ts
+++ query-builder-backend/src/connection-manager/connection-manager.service.spec.ts
@@ -6,6 +6,132 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppService } from '../app.service';
 import { MyLoggerModule } from '../my-logger/my-logger.module';
 
+const SELECT = 0;
+const UPDATE = 1;
+const AUTH_ADMIN = 2;
+const AUTH = 3;
+const INSERT = 4;
+const DELETE = 5;
+const STORAGE = 6;
+
+jest.mock('../supabase/supabase.ts', () => {
+  let testData: any[] = [];
+  let testError: any[] = [];
+
+  return {
+    Supabase: jest.fn().mockImplementation(() => {
+      return {
+        getJwt: jest.fn(),
+        getClient: jest.fn().mockImplementation(() => {
+          return {
+            from: jest.fn().mockReturnThis(),
+            select: jest.fn().mockImplementation(() => ({
+              eq: jest.fn().mockReturnThis(),
+              in: jest.fn().mockReturnThis(),
+              is: jest.fn().mockReturnThis(),
+              order: jest.fn().mockReturnThis(),
+              gte: jest.fn().mockReturnThis(),
+              lte: jest.fn().mockReturnThis(),
+              match: jest.fn().mockReturnThis(),
+              data: testData[SELECT], // Use the data variable here
+              error: testError[SELECT]
+            })),
+            update: jest.fn().mockImplementation(() => ({
+              eq: jest.fn().mockReturnThis(),
+              in: jest.fn().mockReturnThis(),
+              is: jest.fn().mockReturnThis(),
+              order: jest.fn().mockReturnThis(),
+              gte: jest.fn().mockReturnThis(),
+              lte: jest.fn().mockReturnThis(),
+              match: jest.fn().mockReturnThis(),
+              select: jest.fn().mockReturnThis(),
+              data: testData[UPDATE], // Use the data variable here
+              error: testError[UPDATE]
+            })),
+            insert: jest.fn().mockImplementation(() => ({
+              eq: jest.fn().mockReturnThis(),
+              in: jest.fn().mockReturnThis(),
+              is: jest.fn().mockReturnThis(),
+              order: jest.fn().mockReturnThis(),
+              gte: jest.fn().mockReturnThis(),
+              lte: jest.fn().mockReturnThis(),
+              match: jest.fn().mockReturnThis(),
+              select: jest.fn().mockReturnThis(),
+              data: testData[INSERT],
+              error: testError[INSERT]
+            })),
+            upsert: jest.fn().mockImplementation(() => ({
+              eq: jest.fn().mockReturnThis(),
+              in: jest.fn().mockReturnThis(),
+              is: jest.fn().mockReturnThis(),
+              order: jest.fn().mockReturnThis(),
+              gte: jest.fn().mockReturnThis(),
+              lte: jest.fn().mockReturnThis(),
+              match: jest.fn().mockReturnThis(),
+              select: jest.fn().mockReturnThis(),
+              data: testData[INSERT],
+              error: testError[INSERT]
+            })),
+            delete: jest.fn().mockImplementation(() => ({
+              eq: jest.fn().mockReturnThis(),
+              in: jest.fn().mockReturnThis(),
+              is: jest.fn().mockReturnThis(),
+              order: jest.fn().mockReturnThis(),
+              gte: jest.fn().mockReturnThis(),
+              lte: jest.fn().mockReturnThis(),
+              match: jest.fn().mockReturnThis(),
+              select: jest.fn().mockReturnThis(),
+              data: testData[DELETE],
+              error: testError[DELETE]
+            })),
+            storage: {
+              from: jest.fn().mockReturnThis(),
+              upload: jest.fn().mockReturnThis(),
+              getPublicUrl: jest.fn().mockReturnThis(),
+              data: testData[STORAGE],
+              error: testError[STORAGE]
+            },
+            auth: {
+              admin: {
+                createUser: jest.fn().mockReturnThis(),
+                deleteUser: jest.fn().mockReturnThis(),
+                data: testData[AUTH_ADMIN],
+                error: testError[AUTH_ADMIN]
+              },
+              getUser: jest.fn().mockReturnThis(),
+              signUp: jest.fn().mockReturnThis(),
+              signInWithPassword: jest.fn().mockReturnThis(),
+              data: testData[AUTH],
+              error: testError[AUTH]
+            }
+          };
+        })
+      };
+    }),
+    setTestData: (newData: any[]) => {
+      testData = newData;
+    },
+    setTestError: (newError: any[]) => {
+      testError = newError;
+    },
+    getTestData: () => {
+      return testData;
+    },
+    getTestError: () => {
+      return testError;
+    }
+  };
+});
+
+jest.mock('express-session', () => {
+  return {
+    session: jest.fn((req, res, next) => {
+      req.session = {};
+      next();
+    })
+  };
+});
+
 describe('ConnectionManagerService', () => {
   let service: ConnectionManagerService;
 
@@ -26,4 +152,12 @@ describe('ConnectionManagerService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('connectToDatabase', () => {})
+
+  describe('decryptDbSecrets', () => {
+    it('should rethrow the error generated by the Supabase client when the user is not logged in', () => {
+      
+    })
+  })
 });
