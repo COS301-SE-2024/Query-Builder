a5336632b4fe1a7e62be1a3e823e65ee6aed7ef0
Keanumrc <u20693398@tuks.co.za>
2024-08-21 10:55:59 +0200

    Revert "feature(postgres-expansion): added ConnectionManager hierarchy"
    This reverts commit c343643bbff97f0db057dbddb20abe24099b2d1f.

--- query-builder-backend/src/connection-manager/connection-manager.module.ts
+++ query-builder-backend/src/connection-manager/connection-manager.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConnectionManagerService } from './connection-manager.service';
 import { ConnectionManagerController } from './connection-manager.controller';
 import { SupabaseModule } from '../supabase/supabase.module';
 import { ConfigModule } from '@nestjs/config';
 import { AppService } from '../app.service';
 import { SessionStoreModule } from '../session-store/session-store.module';
 import { MyLoggerModule } from '../my-logger/my-logger.module';
-import { PostgresConnectionManagerService } from './postgres-connection-manager/postgres-connection-manager.service';
-import { MySqlConnectionManagerService } from './my-sql-connection-manager/my-sql-connection-manager.service';
 
 @Module({
   imports: [
@@ -16,7 +15,7 @@ import { MySqlConnectionManagerService } from './my-sql-connection-manager/my-sq
     MyLoggerModule
   ],
   controllers: [ConnectionManagerController],
-  providers: [AppService, PostgresConnectionManagerService, MySqlConnectionManagerService],
-  exports: [PostgresConnectionManagerService, MySqlConnectionManagerService]
+  providers: [ConnectionManagerService, AppService],
+  exports: [ConnectionManagerService]
 })
 export class ConnectionManagerModule {}
--- query-builder-backend/src/connection-manager/postgres-connection-manager/postgres-connection-manager.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { ConnectionManagerService, ConnectionStatus } from '../connection-manager.service';
-
-@Injectable()
-export class PostgresConnectionManagerService extends ConnectionManagerService {
-    connectToDatabase(db_id: string, session: Record<string, any>): Promise<ConnectionStatus> {
-        throw new Error('Method not implemented.');
-    }
-}
