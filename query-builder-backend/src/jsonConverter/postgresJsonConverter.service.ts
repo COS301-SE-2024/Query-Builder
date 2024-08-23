721fe959f5a3b386f3573a3a6763abafc0ec44c5
Zane Augustyn <u22528424@tuks.co.za>
2024-08-23 07:03:02 +0200

    feat: Add postgresJsonConverter service for converting JSON to SQL query

---
+++ query-builder-backend/src/jsonConverter/postgresJsonConverter.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@nestjs/common';
+import { condition, compoundCondition, primitiveCondition, QueryParams, table, column } from '../interfaces/intermediateJSON';
+
+@Injectable()
+export class postgresJsonConverter {
+
+    convertJsonToQuery(jsonData: QueryParams): string {
+        let query = '';
+        jsonData.language = jsonData.language.toLowerCase();
+        jsonData.query_type = jsonData.query_type.toLowerCase();
+
+        if (jsonData.language === 'postgres') {
+            if (jsonData.query_type === 'select') {
+                if (!jsonData.table || !jsonData.table.name || !jsonData.table.columns || jsonData.table.columns.length === 0) {
+                    throw new Error('Invalid query');
+                }
+
+                const select = this.generateSelectClausePost(jsonData);
+
+                const from = this.generateFromClausePost(jsonData);
+
+                const where = this.conditionWhereSQLPost(jsonData.condition);
+
+                const groupBy = this.groupBySQLPost(jsonData);
+
+                query = `SELECT ${select} FROM ${from}${where}${groupBy}`;
+
+            } else {
+                throw new Error('Unsupported query type');
+            }
+        }
+        else {
+            throw new Error('Invalid language');
+        }
+
+        return query;
+    }
+
+    generateSelectClausePost(jsonData) {
+        if (!jsonData.table || !jsonData.table.columns) {
+            return '*';
+        }
+        
+        return jsonData.table.columns.map(col => {
+            const columnName = typeof col === 'object' && col.name ? col.name : col;
+            return `"${columnName}"`;
+        }).join(', ');
+    }
+    
+    generateFromClausePost(jsonData) {
+        const tableName = jsonData.table.name;
+        return `"${tableName}"`;
+    }
+    
+    conditionWhereSQLPost(condition) {
+        if (!condition || !condition.column || !condition.operator || condition.value === undefined) return '';
+        
+        const column = `"${condition.column}"`;
+        const operator = condition.operator.toUpperCase();
+        const value = typeof condition.value === 'string' ? `'${condition.value.replace(/'/g, "''")}'` : condition.value; // Handle single quotes in strings
+        
+        return ` WHERE ${column} ${operator} ${value}`;
+    }
+    
+    groupBySQLPost(jsonData) {
+        if (!jsonData.groupBy || jsonData.groupBy.length === 0) return '';
+        
+        return ` GROUP BY ${jsonData.groupBy.map(col => `"${col}"`).join(', ')}`;
+    }
+
+}
