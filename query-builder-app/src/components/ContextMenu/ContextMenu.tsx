f048e93307759fd12aac0d34ffce501d19b4613b
Zane Augustyn <u22528424@tuks.co.za>
2024-08-25 15:19:09 +0200

    chore: Update ContextMenu component with search functionality and adjust CSS height and width

--- query-builder-app/src/components/ContextMenu/ContextMenu.tsx
+++ query-builder-app/src/components/ContextMenu/ContextMenu.tsx
@@ -1,36 +1,32 @@
 import React, { useState } from "react";
-import { Input, ScrollShadow, Spacer } from "@nextui-org/react";
+import { Input, ScrollShadow, Spacer, Button } from "@nextui-org/react";
 import ContextMenuCard from "../ContextMenuCard/ContextMenuCard";
 import { createClient } from "./../../utils/supabase/client";
 
+
+
 export default function ContextMenu() {
 
-
     interface ContextMenuCardProps {
-        queryTitle: string,
-        saved_at: string,
-        parameters: any,
-        query_id: any,
-        db_id: string,
+        queryTitle: string;
+        saved_at: string;
+        parameters: any;
+        query_id: any;
+        db_id: string;
         onDelete: () => void;
     }
 
-    // This function gets the token from local storage.
-    // Supabase stores the token in local storage so we can access it from there.
     const getToken = async () => {
-
         const supabase = createClient();
-        const token = (await supabase.auth.getSession()).data.session?.access_token
-
-        console.log(token)
-
+        const token = (await supabase.auth.getSession()).data.session?.access_token;
+        console.log(token);
         return token;
     };
 
-    function reload()
-    {
+    const reload = () => {
         getSavedQueries();
-    }
+    };
+
 
     async function getSavedQueries() {
         try {
@@ -49,7 +45,6 @@ export default function ContextMenu() {
             }
 
             const data = await response.json();
-
             setSavedQueries(data.query_data);
         } catch (error) {
             console.error("Failed to fetch saved queries:", error);
@@ -57,55 +52,48 @@ export default function ContextMenu() {
         }
     }
 
-
-    //React hook to hold the user's organisations
-    const [savedQueries, setSavedQueries] = React.useState<ContextMenuCardProps[]>([]);
-
+    const [savedQueries, setSavedQueries] = useState<ContextMenuCardProps[]>([]);
     const [searchTerm, setSearchTerm] = useState("");
 
-    //React hook to fetch the user's organisations upon rerender of the component
     React.useEffect(() => {
         getSavedQueries();
-
-    }, [])
-
+    }, []);
 
     const filteredQueries = Array.isArray(savedQueries) 
         ? savedQueries.filter(queryData =>
             queryData.queryTitle.toLowerCase().includes(searchTerm.toLowerCase())
-          )
+        )
         : [];
 
-        return (
-            <div className="size-full pl-3 pr-3 mt-2">
-                <Input 
-                    fullWidth 
-                    color="primary" 
-                    placeholder="Search Queries..." 
-                    onChange={(e) => setSearchTerm(e.target.value)} 
-                    value={searchTerm} 
-                />
-                <Spacer y={2} />
-                <ScrollShadow hideScrollBar style={{ maxHeight: '80%' }}>
-                    {filteredQueries.length > 0 ? (
-                        filteredQueries.map((queryData: ContextMenuCardProps, index: number) => (
-                            <React.Fragment key={index}>
-                                <ContextMenuCard
-                                    queryTitle={queryData.queryTitle}
-                                    saved_at={queryData.saved_at}
-                                    parameters={queryData.parameters}
-                                    query_id={queryData.query_id}
-                                    db_id={queryData.db_id}
-                                    onDelete={reload}
-                                />
-                                <Spacer x={4} />
-                            </React.Fragment>
-                        ))
-                    ) : (
-                        <p>No queries found</p>
-                    )}
-                </ScrollShadow>
-            </div>
-        );
-    
-};
+    return (
+        <div className="size-full pl-3 pr-3 mt-2">
+            <Input 
+                fullWidth 
+                color="primary" 
+                placeholder="Search Queries..." 
+                onChange={(e) => setSearchTerm(e.target.value)} 
+                value={searchTerm} 
+            />
+            <Spacer y={2} />
+            <ScrollShadow hideScrollBar style={{ minHeight: '40vh', maxHeight: '150vh', height: '50vh' }}>
+                {filteredQueries.length > 0 ? (
+                    filteredQueries.map((queryData: ContextMenuCardProps, index: number) => (
+                        <React.Fragment key={index}>
+                            <ContextMenuCard
+                                queryTitle={queryData.queryTitle}
+                                saved_at={queryData.saved_at}
+                                parameters={queryData.parameters}
+                                query_id={queryData.query_id}
+                                db_id={queryData.db_id}
+                                onDelete={reload}
+                            />
+                            <Spacer x={4} />
+                        </React.Fragment>
+                    ))
+                ) : (
+                    <p>No queries found</p>
+                )}
+            </ScrollShadow>
+        </div>
+    );
+}
