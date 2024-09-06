2138f18c4c7ef702d06efc24bde553b89aa3dd77
Keanumrc <u20693398@tuks.co.za>
2024-09-06 15:54:25 +0200

    feature(db-connections): ensure db credentials work before they are saved, added password visibility button to DatabaseCredentialsModal

--- query-builder-app/src/components/DatabaseCredentialsModal/DatabaseCredentialsModal.tsx
+++ query-builder-app/src/components/DatabaseCredentialsModal/DatabaseCredentialsModal.tsx
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input, Checkbox, Tooltip} from "@nextui-org/react";
 import { createClient } from "./../../utils/supabase/client";
 import { navigateToForm } from "@/app/serverActions";
+import { EyeFilledIcon } from "../Authentication/EyeFilledIcon";
+import { EyeSlashFilledIcon } from "../Authentication/EyeSlashFilledIcon";
 
 require("dotenv").config();
 
@@ -34,6 +36,9 @@ export default function DatabaseCredentialsModal(props: DatabaseCredentialsModal
     //React hook to store whether the user wants QBee to remember their database credentials or not
     const [rememberDatabaseCredentials, setRememberDatabaseCredentials] = useState(false);
 
+    //React hook for visibility of the password
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
@@ -54,45 +59,6 @@ export default function DatabaseCredentialsModal(props: DatabaseCredentialsModal
     }, [username]);
 
     async function connectToDatabaseServer(user:string, password:string){
-
-      //if rememberDatabaseCredentials is set, save the db_secrets
-      if(rememberDatabaseCredentials){
-
-        //create a db_secrets object
-        const db_secrets = {
-          user: user,
-          password: password
-        }
-
-        //stringify the db_secrets
-        const db_secrets_string = JSON.stringify(db_secrets);   
-
-        //log request body
-        console.log("BODY OF REQUEST: " + JSON.stringify({
-          db_id: props.dbServerID,
-          db_secrets: db_secrets_string
-        }))
-
-        //call the save-db-secrets API endpoint
-        let response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/org-management/save-db-secrets`, {
-          credentials: "include",
-          method: "POST",
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + await getToken()
-          },
-          body: JSON.stringify({
-              db_id: props.dbServerID,
-              db_secrets: db_secrets_string
-          })
-        });
-
-        //log response body
-        let json = await response.json();
-        console.log("SAVE DB SECRETS RESPONSE " + JSON.stringify(json));
-
-      }
 
       //connect to the database, and navigate to the form if the connection is successful
 
@@ -116,9 +82,50 @@ export default function DatabaseCredentialsModal(props: DatabaseCredentialsModal
 
       let json = await connectionResponse.json();
 
-      //if connection was successful, navigate to the form
+      //if connection was successful, save credentials if necessary, and then navigate to the form
       if(json.success === true){
+
+        //if rememberDatabaseCredentials is set, save the db_secrets
+        if(rememberDatabaseCredentials){
+
+          //create a db_secrets object
+          const db_secrets = {
+            user: user,
+            password: password
+          }
+
+          //stringify the db_secrets
+          const db_secrets_string = JSON.stringify(db_secrets);   
+
+          //log request body
+          console.log("BODY OF REQUEST: " + JSON.stringify({
+            db_id: props.dbServerID,
+            db_secrets: db_secrets_string
+          }))
+
+          //call the save-db-secrets API endpoint
+          let response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/org-management/save-db-secrets`, {
+            credentials: "include",
+            method: "POST",
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + await getToken()
+            },
+            body: JSON.stringify({
+                db_id: props.dbServerID,
+                db_secrets: db_secrets_string
+            })
+          });
+
+          //log response body
+          let json = await response.json();
+          console.log("SAVE DB SECRETS RESPONSE " + JSON.stringify(json));
+
+        }
+
         navigateToForm(props.dbServerID);
+
       }
 
     }
@@ -151,13 +158,26 @@ export default function DatabaseCredentialsModal(props: DatabaseCredentialsModal
                     isRequired
                     label="Password"
                     placeholder="Enter your password"
-                    type="password"
+                    type={isPasswordVisible ? "text" : "password"}
                     variant="bordered"
                     onValueChange={setPassword}
                     onFocus={() => setPasswordHasBeenFocused(true)}
                     isInvalid={isPasswordInvalid && passwordBeenFocused}
                     color={!passwordBeenFocused ? "primary" : isPasswordInvalid ? "danger" : "success"}
                     errorMessage="Please enter a password"
+                    endContent={
+                      <button 
+                        className="focus:outline-none" 
+                        type="button" 
+                        onClick={() => {setIsPasswordVisible(!isPasswordVisible)}} 
+                        aria-label="toggle password visibility">
+                        {isPasswordVisible ? (
+                          <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                        ) : (
+                          <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                        )}
+                      </button>
+                    }
                   />
                   <Checkbox
                     isSelected={rememberDatabaseCredentials}
