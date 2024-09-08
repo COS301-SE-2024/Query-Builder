6c4e0c57f6838bf43dc29bba669eb8d2d9aba125
Keanumrc <u20693398@tuks.co.za>
2024-09-08 23:25:28 +0200

    testing(db-connections): added query test

--- query-builder-app/src/components/SignedInHomePage/SignedInHomePage.spec.tsx
+++ query-builder-app/src/components/SignedInHomePage/SignedInHomePage.spec.tsx
@@ -141,4 +141,70 @@ describe('SignedInHomePage querying tests', () => {
 
   });
 
+  it('should be able to query a database that has no active connection, but db secrets saved', async () => {
+
+    //Mock out the API calls
+    global.fetch = vi.fn((url: string, config: any) => {
+
+      if(url == `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/org-management/get-org`){
+          return Promise.resolve({
+                ok: true,
+                json: () => Promise.resolve({data: [{
+                  created_at: "",
+                  logo: "",
+                  name: "Mock Org",
+                  org_id: "MockOrgID",
+                  db_envs: [
+                    {
+                      created_at: "",
+                      name: "Mock Database Server",
+                      db_id: "MockDBID",
+                      db_info: "",
+                      type: "mysql"
+                    }
+                  ],
+                  org_members: [
+                    "Member1"
+                  ]
+                }]}),
+          });
+      }
+      else if(url == `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/has-active-connection`){
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve({hasActiveConnection: false}),
+        });
+      }
+      else if(url == `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/org-management/has-saved-db-credentials`){
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve({saved_db_credentials: true}),
+        });
+      }
+      else if(url == `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/connect`){
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve({success: true}),
+        });
+      }
+
+    }) as Mock;
+
+    //create a user that can perform actions
+    const user = userEvent.setup();
+
+    //render the component
+    render(<SignedInHomePage/>);
+
+    //find the database server link to click
+    const serverText = (await screen.findAllByText('Mock Database Server'))[0];
+
+    //click the database server link
+    await user.click(serverText);
+
+    //expect to be redirected to the query form
+    expect(ServerActions.navigateToForm).toBeCalled();
+
+  });
+
 });
