6a5c797ec8906337f08251614a02705923791daa
GremBleen <graemeharryblain@gmail.com>
2024-09-12 00:17:41 +0200

    Integrated JsonConverter changes

--- query-builder-backend/src/query-handler/query-handler.service.spec.ts
+++ query-builder-backend/src/query-handler/query-handler.service.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QueryHandlerService } from './query-handler.service';
 import { SessionStoreModule } from '../session-store/session-store.module';
-import { JsonConverterModule } from '../jsonConverter/jsonConverter.module';
+import { JsonConverterModule } from '../json-converter/json-converter.module';
 import { ConnectionManagerModule } from '../connection-manager/connection-manager.module';
 import { MyLoggerModule } from '../my-logger/my-logger.module';
 
@@ -10,7 +10,11 @@ describe('QueryHandlerService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [JsonConverterModule, ConnectionManagerModule, MyLoggerModule],
+      imports: [
+        JsonConverterModule.forRoot('mysql'),
+        ConnectionManagerModule,
+        MyLoggerModule
+      ],
       providers: [QueryHandlerService]
     }).compile();
 
