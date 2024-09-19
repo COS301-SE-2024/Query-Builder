7ae74b19c98a1586c50e9ab6369beed7b89ee567
Zane Augustyn <u22528424@tuks.co.za>
2024-09-19 15:01:48 +0200

    Refactor Form component to remove unused imports and update API calls

--- query-builder-app/src/components/Form/Form.tsx
+++ query-builder-app/src/components/Form/Form.tsx
@@ -338,7 +338,7 @@ export default function Form() {
                                     }}                                >
                                     Clear Form
                                 </Button>
-                                {JSON.stringify(query, null, 2)}
+                                {/* {JSON.stringify(query, null, 2)} ----------------------------------------------------------------------------------*/}
                             </div>
                             <Modal
                                 isOpen={isOpen}
