012593b7c723302ec3a98ec97cbc592f1936ad47
Keanumrc <u20693398@tuks.co.za>
2024-09-20 10:11:14 +0200

    feature(postgres-integration): added class validation to DbMetadataHandler

--- query-builder-backend/src/db-metadata-handler/db-metadata-handler.controller.ts
+++ query-builder-backend/src/db-metadata-handler/db-metadata-handler.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Inject, Put, Session } from '@nestjs/common';
+import { Body, Controller, Inject, Put, Session, ValidationPipe } from '@nestjs/common';
 import { DbMetadataHandlerService } from './db-metadata-handler.service';
 import { Database_Metadata_Dto, Field_Metadata_Dto, Foreign_Key_Metadata_Dto, Server_Summary_Metadata_Dto, Table_Metadata_Dto } from './dto/metadata.dto';
 
@@ -8,27 +8,27 @@ export class DbMetadataHandlerController {
     constructor(@Inject('DbMetadataHandlerService') private readonly dbMetadataHandlerService: DbMetadataHandlerService) {}
 
     @Put("databases")
-    async getSchemaMetadata(@Body() databaseMetadataDto: Database_Metadata_Dto, @Session() session: Record<string, any>) {
+    async getSchemaMetadata(@Body(ValidationPipe) databaseMetadataDto: Database_Metadata_Dto, @Session() session: Record<string, any>) {
         return this.dbMetadataHandlerService.getDatabaseMetadata(databaseMetadataDto, session);
     }
 
     @Put("tables")
-    async getTableMetadata(@Body() tableMetadataDto: Table_Metadata_Dto, @Session() session: Record<string, any>) {
+    async getTableMetadata(@Body(ValidationPipe) tableMetadataDto: Table_Metadata_Dto, @Session() session: Record<string, any>) {
         return this.dbMetadataHandlerService.getTableMetadata(tableMetadataDto, session);
     }
 
     @Put("fields")
-    async getFieldMetadata(@Body() fieldMetadataDto: Field_Metadata_Dto, @Session() session: Record<string, any>){
+    async getFieldMetadata(@Body(ValidationPipe) fieldMetadataDto: Field_Metadata_Dto, @Session() session: Record<string, any>){
         return this.dbMetadataHandlerService.getFieldMetadata(fieldMetadataDto, session);
     }
 
     @Put("foreign-keys")
-    async getForeignKeyMetadata(@Body() foreignKeyMetadataDto: Foreign_Key_Metadata_Dto, @Session() session: Record<string, any>){
+    async getForeignKeyMetadata(@Body(ValidationPipe) foreignKeyMetadataDto: Foreign_Key_Metadata_Dto, @Session() session: Record<string, any>){
         return this.dbMetadataHandlerService.getForeignKeyMetadata(foreignKeyMetadataDto, session);
     }
 
     @Put("summary")
-    async getSchemaSummary(@Body() serverSummaryMetadataDto: Server_Summary_Metadata_Dto, @Session() session: Record<string, any>){
+    async getSchemaSummary(@Body(ValidationPipe) serverSummaryMetadataDto: Server_Summary_Metadata_Dto, @Session() session: Record<string, any>){
         return this.dbMetadataHandlerService.getServerSummary(serverSummaryMetadataDto, session);
     }
 }
