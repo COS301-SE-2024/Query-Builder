b0b8095ae67da5d13c2cdfcc76613e6fb8c1b5e1
Keanumrc <u20693398@tuks.co.za>
2024-09-20 13:20:09 +0200

    feature(postgres-integration): got /fields endpoint working

--- query-builder-backend/src/db-metadata-handler/postgres-db-metadata-handler/postgres-db-metadata-handler.service.ts
+++ query-builder-backend/src/db-metadata-handler/postgres-db-metadata-handler/postgres-db-metadata-handler.service.ts
@@ -3,7 +3,7 @@ import { DbMetadataHandlerService } from '../db-metadata-handler.service';
 import { Database_Metadata_Dto, Table_Metadata_Dto, Field_Metadata_Dto, Foreign_Key_Metadata_Dto, Server_Summary_Metadata_Dto } from '../dto/metadata.dto';
 import { Query } from './../../interfaces/dto/query.dto';
 import { column } from 'src/interfaces/dto/table.dto';
-import { ComparisonOperator, primitiveCondition } from 'src/interfaces/dto/conditions.dto';
+import { ComparisonOperator, compoundCondition, LogicalOperator, primitiveCondition } from 'src/interfaces/dto/conditions.dto';
 
 @Injectable()
 export class PostgresDbMetadataHandlerService extends DbMetadataHandlerService {
@@ -77,8 +77,49 @@ export class PostgresDbMetadataHandlerService extends DbMetadataHandlerService {
           return response.data;
 
     }
-    getFieldMetadata(fieldMetadataDto: Field_Metadata_Dto, session: Record<string, any>): Promise<any> {
-        throw new Error('Method not implemented.');
+
+    async getFieldMetadata(fieldMetadataDto: Field_Metadata_Dto, session: Record<string, any>): Promise<any> {
+
+        /*
+        SELECT column_name
+        FROM information_schema.columns
+        WHERE table_schema = 'public' AND table_name='----------determined by input----------'
+        ORDER BY column_name;
+        */
+
+        const query: Query = {
+            databaseServerID: fieldMetadataDto.databaseServerID,
+            queryParams: {
+              language: 'postgresql',
+              query_type: 'select',
+              databaseName: fieldMetadataDto.database,
+              table: {
+                name: 'information_schema"."columns',
+                columns: [{ name: 'column_name', alias: 'name' }]
+              },
+              condition: {
+                operator: LogicalOperator.AND,
+                conditions: [
+                  {
+                    column: 'table_schema',
+                    operator: ComparisonOperator.EQUAL,
+                    value: 'public'
+                  },
+                  {
+                    column: 'table_name',
+                    operator: ComparisonOperator.EQUAL,
+                    value: fieldMetadataDto.table
+                  }
+                ]
+              } as unknown as compoundCondition,
+              sortParams: {
+                column: 'column_name'
+              }
+            }
+        };
+    
+        return await this.queryHandlerService.queryDatabase(query, session);
+
     }
     getForeignKeyMetadata(foreignKeyMetadataDto: Foreign_Key_Metadata_Dto, session: Record<string, any>): Promise<any> {
         throw new Error('Method not implemented.');
