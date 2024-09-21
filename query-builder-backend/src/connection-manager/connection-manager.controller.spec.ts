2b2345bf5c83647c68e47ebfc4aa6bb3b61e8739
Keanumrc <u20693398@tuks.co.za>
2024-09-21 19:25:53 +0200

    testing(postgres-integration): got connection-manager tests working

--- query-builder-backend/src/connection-manager/connection-manager.controller.spec.ts
+++ query-builder-backend/src/connection-manager/connection-manager.controller.spec.ts
@@ -1,42 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConnectionManagerController } from './connection-manager.controller';
-import { ConnectionManagerService } from './connection-manager.service';
-import { SessionStoreModule } from '../session-store/session-store.module';
-import { SupabaseModule } from '../supabase/supabase.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AppService } from '../app.service';
-import { MyLoggerModule } from '../my-logger/my-logger.module';
-import { MySqlConnectionManagerService } from './my-sql-connection-manager/my-sql-connection-manager.service';
-import { PostgresConnectionManagerService } from './postgres-connection-manager/postgres-connection-manager.service';
-
-class MockConnectionManagerService {
-  connectToDatabase() {
-    return 'mocked connection';
-  }
-  hasActiveConnection() {
-    return 'mocked active connection';
-  }
-}
+import { Connect_Dto } from './dto/connect.dto';
 
 describe('ConnectionManagerController', () => {
   let controller: ConnectionManagerController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        SupabaseModule,
-        ConfigModule.forRoot({ isGlobal: true }),
-        SessionStoreModule,
-        MyLoggerModule
-      ],
       controllers: [ConnectionManagerController],
       providers: [
         {
-          provide: ConnectionManagerService,
-          useClass: MockConnectionManagerService
-        },
-        AppService,
-        ConfigService
+          provide: 'ConnectionManagerService',
+          useValue: {
+            connectToDatabase(connect_dto: Connect_Dto, session: Record<string, any>){}
+          }
+        }
       ]
     }).compile();
 
@@ -49,17 +27,4 @@ describe('ConnectionManagerController', () => {
     expect(controller).toBeDefined();
   });
 
-  describe('connect', () => {
-    it('should return "mocked connection"', async () => {
-      expect(await controller.connect(null, null)).toBe('mocked connection');
-    });
-  });
-
-  describe('hasActiveConnection', () => {
-    it('should return "mocked active connection"', async () => {
-      expect(await controller.hasActiveConnection(null, null)).toBe(
-        'mocked active connection'
-      );
-    });
-  });
 });
