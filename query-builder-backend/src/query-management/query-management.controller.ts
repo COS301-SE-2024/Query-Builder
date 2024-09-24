c6a09169e1baf63fe9e6c817fcb992380da43598
GremBleen <graemeharryblain@gmail.com>
2024-09-24 13:42:15 +0200

    Refactor query-management controller and service
    - Add new endpoint to get shareable members
    - Import Get_Shareable_Members_Dto in query-management controller
    - Implement getShareableMembers method in query-management service

--- query-builder-backend/src/query-management/query-management.controller.ts
+++ query-builder-backend/src/query-management/query-management.controller.ts
@@ -3,6 +3,7 @@ import { Save_Query_Dto } from './dto/save-query.dto';
 import { Delete_Query_Dto } from './dto/delete-query.dto';
 import { QueryManagementService } from './query-management.service';
 import { Get_Single_Query_Dto } from './dto/get-single-query.dto';
+import { Get_Shareable_Members_Dto } from './dto/get-shareable-members.dto';
 
 @Controller('query-management')
 export class QueryManagementController {
@@ -27,6 +28,11 @@ export class QueryManagementController {
   @Post("delete-query")
   async deleteQ(@Body(ValidationPipe) delete_query_dto: Delete_Query_Dto) {
     return this.query_management_service.deleteQuery(delete_query_dto);
+  }
+
+  @Put("get-shareable-members")
+  async getShareableMembers(@Body(ValidationPipe) get_shareable_members_dto: Get_Shareable_Members_Dto) {
+    return this.query_management_service.getShareableMembers(get_shareable_members_dto);
   }
 
 }
--- query-builder-backend/src/query-management/query-management.service.ts
+++ query-builder-backend/src/query-management/query-management.service.ts
@@ -1,119 +1,170 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException
+} from '@nestjs/common';
 import { Supabase } from '../supabase';
 import { Save_Query_Dto } from './dto/save-query.dto';
 import { Delete_Query_Dto } from './dto/delete-query.dto';
 import { query } from 'express';
 import { Get_Single_Query_Dto } from './dto/get-single-query.dto';
+import { Get_Shareable_Members_Dto } from './dto/get-shareable-members.dto';
+import { NotFoundError } from 'openai';
+import { first } from 'rxjs';
 
 @Injectable()
 export class QueryManagementService {
+  constructor(private readonly supabase: Supabase) {}
 
-    constructor(private readonly supabase: Supabase,) { }
+  async saveQuery(save_query_dto: Save_Query_Dto) {
+    //Firstly get the user who is saving the query
+    const { data: user_data, error: user_error } = await this.supabase
+      .getClient()
+      .auth.getUser(this.supabase.getJwt());
 
-    async saveQuery(save_query_dto: Save_Query_Dto) {
-
-        //Firstly get the user who is saving the query
-        const { data: user_data, error: user_error } = await this.supabase
-            .getClient()
-            .auth.getUser(this.supabase.getJwt());
-
-        if (user_error) {
-            throw user_error;
-        }
-
-        //Secondly add the query to the saved_queries table
-        const saved_query_fields = {
-            parameters: save_query_dto.parameters,
-            user_id: user_data.user.id,
-            db_id: save_query_dto.db_id,
-            queryTitle: save_query_dto.queryTitle
-        }
-
-        const { data: save_data, error: save_error } = await this.supabase
-            .getClient()
-            .from('saved_queries')
-            .insert({ ...saved_query_fields })
-            .select();
-
-        if (save_error) {
-            throw save_error;
-        }
-        if (save_data.length === 0) {
-            throw new InternalServerErrorException('Query not saved');
-        }
-
-        return { save_data };
-
+    if (user_error) {
+      throw user_error;
     }
 
-    async getQueries() {
+    //Secondly add the query to the saved_queries table
+    const saved_query_fields = {
+      parameters: save_query_dto.parameters,
+      user_id: user_data.user.id,
+      db_id: save_query_dto.db_id,
+      queryTitle: save_query_dto.queryTitle
+    };
 
-        //Firstly get the user who is saving the query
-        const { data: user_data, error: user_error } = await this.supabase
-            .getClient()
-            .auth.getUser(this.supabase.getJwt());
+    const { data: save_data, error: save_error } = await this.supabase
+      .getClient()
+      .from('saved_queries')
+      .insert({ ...saved_query_fields })
+      .select();
 
-        if (user_error) {
-            throw user_error;
-        }
-
-        //Secondly get the queries saved by the user
-        const { data: query_data, error: query_error } = await this.supabase
-            .getClient()
-            .from('saved_queries')
-            .select('parameters, queryTitle, saved_at, query_id, db_id')
-            .eq('user_id', user_data.user.id);
-        if (query_error) {
-            throw query_error;
-        }
-
-        return { query_data };
+    if (save_error) {
+      throw save_error;
+    }
+    if (save_data.length === 0) {
+      throw new InternalServerErrorException('Query not saved');
     }
 
-    async getSingleQuery(get_single_query_dto: Get_Single_Query_Dto) {
+    return { save_data };
+  }
 
-        //Firstly get the user who is saving the query
-        const { data: user_data, error: user_error } = await this.supabase
-            .getClient()
-            .auth.getUser(this.supabase.getJwt());
+  async getQueries() {
+    //Firstly get the user who is saving the query
+    const { data: user_data, error: user_error } = await this.supabase
+      .getClient()
+      .auth.getUser(this.supabase.getJwt());
 
-        if (user_error) {
-            throw user_error;
-        }
-
-        //Secondly get the query saved by the user
-        const { data: query_data, error: query_error } = await this.supabase
-            .getClient()
-            .from('saved_queries')
-            .select('parameters, queryTitle, saved_at, query_id, db_id')
-            .eq('user_id', user_data.user.id).eq('query_id', get_single_query_dto.query_id).single();
-        if (query_error) {
-            throw query_error;
-        }
-
-        return query_data;
+    if (user_error) {
+      throw user_error;
     }
 
-
-    async deleteQuery(delete_query_dto: Delete_Query_Dto) {
-        try {
-            // Deleting the query from the saved_queries table
-            const { data: delete_data, error: delete_error } = await this.supabase
-                .getClient()
-                .from('saved_queries')
-                .delete()
-                .eq('query_id', delete_query_dto.query_id);
-
-            if (delete_error) {
-                throw new Error(`Query deletion error: ${delete_error.message}`);
-            }
-
-            return { delete_data };
-
-        } catch (error) {
-            console.error("Error deleting query:", error);
-            throw error;
-        }
+    //Secondly get the queries saved by the user
+    const { data: query_data, error: query_error } = await this.supabase
+      .getClient()
+      .from('saved_queries')
+      .select('parameters, queryTitle, saved_at, query_id, db_id')
+      .eq('user_id', user_data.user.id);
+    if (query_error) {
+      throw query_error;
     }
 
+    return { query_data };
+  }
+
+  async getSingleQuery(get_single_query_dto: Get_Single_Query_Dto) {
+    //Firstly get the user who is saving the query
+    const { data: user_data, error: user_error } = await this.supabase
+      .getClient()
+      .auth.getUser(this.supabase.getJwt());
+
+    if (user_error) {
+      throw user_error;
+    }
+
+    //Secondly get the query saved by the user
+    const { data: query_data, error: query_error } = await this.supabase
+      .getClient()
+      .from('saved_queries')
+      .select('parameters, queryTitle, saved_at, query_id, db_id')
+      .eq('user_id', user_data.user.id)
+      .eq('query_id', get_single_query_dto.query_id)
+      .single();
+    if (query_error) {
+      throw query_error;
+    }
+
+    return query_data;
+  }
+
+  async deleteQuery(delete_query_dto: Delete_Query_Dto) {
+    try {
+      // Deleting the query from the saved_queries table
+      const { data: delete_data, error: delete_error } = await this.supabase
+        .getClient()
+        .from('saved_queries')
+        .delete()
+        .eq('query_id', delete_query_dto.query_id);
+
+      if (delete_error) {
+        throw new Error(`Query deletion error: ${delete_error.message}`);
+      }
+
+      return { delete_data };
+    } catch (error) {
+      console.error('Error deleting query:', error);
+      throw error;
+    }
+  }
+
+  async getShareableMembers(
+    get_shareable_members_dto: Get_Shareable_Members_Dto
+  ) {
+    const { data: user_data, error: user_error } = await this.supabase
+      .getClient()
+      .auth.getUser(this.supabase.getJwt());
+
+    if (user_error) {
+      throw user_error;
+    }
+
+    const { data: org_data, error: org_error } = await this.supabase
+      .getClient()
+      .from('db_envs')
+      .select('org_id')
+      .eq('db_id', get_shareable_members_dto.db_id)
+      .single();
+
+    if (org_error) {
+      throw org_error;
+    }
+    if (!org_data) {
+      throw new NotFoundException('Organization not found');
+    }
+
+    const { data: members_data, error: members_error } = await this.supabase
+      .getClient()
+      .from('org_members')
+      .select('user_id, profiles(*)')
+      .eq('org_id', org_data.org_id)
+      .neq('user_id', user_data.user.id);
+
+    if (members_error) {
+      throw members_error;
+    }
+
+    // unpack the members data such that it is an array of objects with the keys user_id and first_name and last_name
+    const updated_members_data = members_data.map((member) => {
+        const profiles: any = member.profiles;
+      return {
+        user_id: member.user_id,
+        first_name: profiles.first_name,
+        last_name: profiles.last_name,
+        profile_photo: profiles.profile_photo
+      };
+    });
+
+    return { data: updated_members_data };
+  }
 }
