2ea1bcccb1a962637b106fddee6aa9b7cd2add70
Zane Augustyn <u22528424@tuks.co.za>
2024-09-27 13:02:28 +0200

    Refactor password reset page and form component

---
+++ query-builder-app/src/app/reset/page.tsx
@@ -0,0 +1,96 @@
+'use client';
+import { useState } from 'react';
+import { createClient } from '../../utils/supabase/client';
+import { useRouter } from 'next/navigation';
+import { Button, Input } from '@nextui-org/react';
+
+export default function Page() {
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const router = useRouter();
+
+  const supabase = createClient();
+
+  const handlePasswordReset = async () => {
+    if (password === confirmPassword) {
+      setError(''); // Clear error
+      const success = await sendResetPassword();
+      if (success) {
+        alert('Password has been reset successfully!');
+        router.push('/signedInHome'); // Redirect user after successful reset
+      }
+    } else {
+      setError('Passwords do not match');
+    }
+  };
+
+  const sendResetPassword = async () => {
+    try {
+      const { error } = await supabase.auth.updateUser({
+        password,
+      });
+
+      if (error) {
+        setError(error.message);
+        return false;
+      }
+      return true;
+    } catch (error) {
+      console.error(error);
+      setError('An unexpected error occurred.');
+      return false;
+    }
+  };
+
+  return (
+    <div className="bg-gray-800 bg-opacity-50 h-screen flex items-center justify-center">
+      <div className="bg-white shadow-2xl rounded-2xl p-8 w-[400px]">
+        <div className="flex justify-center">
+          <h1 className="text-4xl font-bold text-gray-900">Reset Password</h1>
+        </div>
+        <div className="grid gap-4 mt-6">
+          <div className="grid">
+            <label className="text-gray-700">Enter your password:</label>
+            <Input
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="p-2 rounded-lg "
+            />
+          </div>
+          <div className="grid">
+            <label className="text-gray-700">Confirm your password:</label>
+            <Input
+              type={showPassword ? 'text' : 'password'}
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="p-2 rounded-lg"
+            />
+          </div>
+          <div className="flex items-center text-sm mt-2">
+            <input
+              type="checkbox"
+              name="showPassword"
+              checked={showPassword}
+              onChange={(e) => setShowPassword(e.target.checked)}
+              className="mr-2"
+            />
+            <label className="text-gray-600">Show password</label>
+          </div>
+          {error && <p className="text-red-500 mt-2">{error}</p>}
+          <Button 
+            color='primary'
+            onClick={handlePasswordReset}
+            className="w-full mt-4"
+          >
+            Reset Password
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
