677367e1d2194ad92abe627dc04b46e724aa961d
Zane Augustyn <u22528424@tuks.co.za>
2024-09-27 17:54:45 +0200

    Refactor toast notifications in authentication and organisation pages

--- query-builder-app/src/app/authentication/page.tsx
+++ query-builder-app/src/app/authentication/page.tsx
@@ -5,30 +5,30 @@ import { Toaster } from 'react-hot-toast';
 export default function Auth() {
   return (
     <>
-    <Toaster
-    position="top-center"
-    reverseOrder={false}
-    gutter={8}
-    containerClassName=""
-    containerStyle={{}}
-    toastOptions={{
-        // Define default options
-        className: '',
-        duration: 5000,
-        style: {
-        color: '#000',
-        background: '#fff',
-        },
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: '',
+          duration: 5000,
+          style: {
+            color: '#000',
+            background: '#fff',
+          },
 
-        // Default options for specific types
-        success: {
-        duration: 3000,
-        iconTheme: {
-            primary: 'green',
-            secondary: 'black',
-        },
-        },
-    }}/>
+          // Default options for specific types
+          success: {
+            duration: 3000,
+            iconTheme: {
+              primary: 'green',
+              secondary: 'black',
+            },
+          },
+        }} />
     <LandingPage />
   </>);
 }
