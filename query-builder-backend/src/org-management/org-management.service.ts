ba83f9d397f25ea6504631917e11448eed6e5b57
Keanumrc <u20693398@tuks.co.za>
2024-09-28 12:49:43 +0200

    removed helper function separation

--- query-builder-backend/src/org-management/org-management.service.ts
+++ query-builder-backend/src/org-management/org-management.service.ts
@@ -73,27 +73,9 @@ export class OrgManagementService {
     return { data };
   }
 
-  async getOrgLoggedIn_H1(org_ids) {
-    const { data: org_data, error: org_data_error } = await this.supabase
-      .getClient()
-      .from('organisations')
-      .select('org_id, created_at, name, logo, org_members(*), db_envs(*)')
-      .in(
-        'org_id',
-        org_ids.map((org) => org.org_id)
-      );
-
-    if (org_data_error) {
-      throw org_data_error;
-    }
-    if (org_data.length === 0) {
-      throw new NotFoundException('Organisation not found');
-    }
-
-    return { org_data };
-  }
-
   async getOrgLoggedIn() {
+
+    //get the currently logged-in user
     const { data, error } = await this.supabase
       .getClient()
       .auth.getUser(this.supabase.getJwt());
@@ -102,13 +84,14 @@ export class OrgManagementService {
       throw error;
     }
 
+    //get the id of the currently logged-in user
     const user_id = data.user.id;
 
+    //get the org IDs of all the organisations 
     const { data: org_ids, error: org_error } = await this.supabase
       .getClient()
       .from('org_members')
       .select('org_id')
-      // .select('org_id, created_at, name, logo, org_members(*), db_envs(*)')
       .eq('user_id', user_id);
 
     if (org_error) {
@@ -118,9 +101,24 @@ export class OrgManagementService {
       throw new NotFoundException('Organisation not found');
     }
 
-    const { org_data } = await this.getOrgLoggedIn_H1(org_ids);
+    const { data: org_data, error: org_data_error } = await this.supabase
+    .getClient()
+    .from('organisations')
+    .select('org_id, created_at, name, logo, org_members(*), db_envs(*)')
+    .in(
+      'org_id',
+      org_ids.map((org) => org.org_id)
+    );
+
+    if (org_data_error) {
+      throw org_data_error;
+    }
+    if (org_data.length === 0) {
+      throw new NotFoundException('Organisation not found');
+    }
 
     return { data: org_data };
+
   }
 
   async getMembers_H1(orgId: string, userId: string) {
