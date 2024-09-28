442d5bc0b4de29d80c760ca587695fdeb7f4d93d
GremBleen <graemeharryblain@gmail.com>
2024-09-28 10:52:51 +0200

    refactor(metadata): Update saveDbMetadata and getSavedDbMetadata functions
    This commit refactors the saveDbMetadata and getSavedDbMetadata functions in the PostgresDbMetadataHandlerService. The changes include:
    - Adding user authentication and permission checks
    - Retrieving and updating existing metadata
    - Processing and returning the metadata in a specific format
    
    Refactoring these functions improves the security and functionality of the metadata handling in the application.

--- query-builder-backend/src/db-metadata-handler/postgres-db-metadata-handler/postgres-db-metadata-handler.service.ts
+++ query-builder-backend/src/db-metadata-handler/postgres-db-metadata-handler/postgres-db-metadata-handler.service.ts
@@ -15,9 +15,18 @@ import {
   primitiveCondition
 } from './../../interfaces/dto/conditions.dto';
 import { Saved_DB_Metadata_Dto } from '../dto/save-metadata.dto';
+import { Supabase } from '../../supabase/supabase';
+import { QueryHandlerService } from '../../query-handler/query-handler.service';
 
 @Injectable()
 export class PostgresDbMetadataHandlerService extends DbMetadataHandlerService {
+  constructor(
+    private readonly supabase: Supabase,
+    protected readonly queryHandlerService: QueryHandlerService
+  ) {
+    super(queryHandlerService);
+  }
+
   async getDatabaseMetadata(
     databaseMetadataDto: Database_Metadata_Dto,
     session: Record<string, any>
@@ -330,22 +339,376 @@ export class PostgresDbMetadataHandlerService extends DbMetadataHandlerService {
     return response;
   }
 
-  saveDbMetadata(save_db_metadata_dto: Saved_DB_Metadata_Dto) {
-    throw new Error('Method not implemented.');
+  async saveDbMetadata(save_db_metadata_dto: Saved_DB_Metadata_Dto) {
+    // Get the user information
+    const { data: user_data, error: user_error } = await this.supabase
+      .getClient()
+      .auth.getUser(this.supabase.getJwt());
+
+    if (user_error) {
+      throw user_error;
+    }
+
+    // Get the org_id from the database
+    if (!save_db_metadata_dto.org_id) {
+      const { data: org_data, error: org_error } = await this.supabase
+        .getClient()
+        .from('db_envs')
+        .select('org_id')
+        .eq('db_id', save_db_metadata_dto.databaseServerID)
+        .single();
+
+      if (org_error) {
+        throw org_error;
+      }
+
+      save_db_metadata_dto.org_id = org_data.org_id;
+    }
+
+    // Check if the user has permission to save the metadata
+    const { data: permission_data, error: permission_error } =
+      await this.supabase
+        .getClient()
+        .from('org_members')
+        .select('user_role')
+        .eq('org_id', save_db_metadata_dto.org_id)
+        .eq('user_id', user_data.user.id)
+        .single();
+
+    if (permission_error) {
+      throw permission_error;
+    }
+    if (!permission_data || permission_data.user_role === 'member') {
+      throw new Error('User does not have permission to save metadata');
+    }
+
+    // Retrieve existing metadata
+    const { data: existing_data, error: existing_error } = await this.supabase
+      .getClient()
+      .from('db_envs')
+      .select('db_info')
+      .eq('db_id', save_db_metadata_dto.databaseServerID)
+      .single();
+
+    if (existing_error) {
+      throw existing_error;
+    }
+
+    // Combine the existing metadata with the new metadata. first check if the db_info is empty
+    if (!existing_data.db_info) {
+      existing_data.db_info = {
+        databases: [
+          {
+            ...save_db_metadata_dto.db_metadata
+          }
+        ]
+      };
+    } else {
+      // Check if the database already exists
+      const existing_db = existing_data.db_info.databases.find(
+        (db) => db.schema_name === save_db_metadata_dto.db_metadata.schema_name
+      );
+
+      if (existing_db) {
+        // Update the existing database
+        if (save_db_metadata_dto.db_metadata.tables) {
+          for (const new_table of save_db_metadata_dto.db_metadata.tables) {
+            const existing_table = existing_db.tables.find(
+              (table) => table.table_name === new_table.table_name
+            );
+
+            if (existing_table) {
+              // Update the existing table
+              existing_table.description = new_table.description;
+
+              if (new_table.fields && existing_table.fields) {
+                for (const new_field of new_table.fields) {
+                  const existing_field = existing_table.fields.find(
+                    (field) => field.column_name === new_field.column_name
+                  );
+
+                  if (existing_field) {
+                    // Update the existing field
+                    existing_field.description = new_field.description;
+                  } else {
+                    // Add the new field
+                    existing_table.fields.push({
+                      column_name: new_field.column_name,
+                      description: new_field.description
+                    });
+                  }
+                }
+              } else {
+                // Add the new fields
+                existing_table.fields = new_table.fields;
+              }
+            } else {
+              // Add the new table
+              existing_db.tables.push({
+                ...new_table
+              });
+            }
+          }
+        }
+      } else {
+        // Add the new database
+        existing_data.db_info.databases.push({
+          ...save_db_metadata_dto.db_metadata
+        });
+      }
+    }
+
+    // Insert the metadata
+    const { data, error } = await this.supabase
+      .getClient()
+      .from('db_envs')
+      .update({
+        db_info: existing_data.db_info
+      })
+      .eq('db_id', save_db_metadata_dto.databaseServerID)
+      .select();
+
+    if (error) {
+      throw error;
+    }
+
+    return { data: data };
   }
-  getSavedDbMetadata(get_db_metadata_dto: Database_Metadata_Dto) {
-    throw new Error('Method not implemented.');
+
+  async getSavedDbMetadata(get_db_metadata_dto: Database_Metadata_Dto) {
+    // Get the user information
+    const { data: user_data, error: user_error } = await this.supabase
+      .getClient()
+      .auth.getUser(this.supabase.getJwt());
+
+    if (user_error) {
+      throw user_error;
+    }
+
+    // Get the org_id from the database
+    const { data: org_data, error: org_error } = await this.supabase
+      .getClient()
+      .from('db_envs')
+      .select('org_id')
+      .eq('db_id', get_db_metadata_dto.databaseServerID)
+      .single();
+
+    if (org_error) {
+      throw org_error;
+    }
+
+    // Get the metadata
+    const { data: metadata_data, error: metadata_error } = await this.supabase
+      .getClient()
+      .from('db_envs')
+      .select('db_info')
+      .eq('db_id', get_db_metadata_dto.databaseServerID)
+      .single();
+
+    if (metadata_error) {
+      throw metadata_error;
+    }
+
+    // Process the metadata, such that it only returns the schema_name and the description of each database
+    if (!metadata_data.db_info) {
+      return { data: [] };
+    }
+
+    const processed_data = metadata_data.db_info.databases.map((db) => {
+      return {
+        database: db.schema_name,
+        description: db.description
+      };
+    });
+
+    return { data: processed_data };
   }
-  getSavedTableMetadata(get_table_metadata_dto: Table_Metadata_Dto) {
-    throw new Error('Method not implemented.');
+
+  async getSavedTableMetadata(get_table_metadata_dto: Table_Metadata_Dto) {
+    // Get the user information
+    const { data: user_data, error: user_error } = await this.supabase
+      .getClient()
+      .auth.getUser(this.supabase.getJwt());
+
+    if (user_error) {
+      throw user_error;
+    }
+
+    // Get the org_id from the database
+    const { data: org_data, error: org_error } = await this.supabase
+      .getClient()
+      .from('db_envs')
+      .select('org_id')
+      .eq('db_id', get_table_metadata_dto.databaseServerID)
+      .single();
+
+    if (org_error) {
+      throw org_error;
+    }
+
+    // Get the metadata
+    const { data: metadata_data, error: metadata_error } = await this.supabase
+      .getClient()
+      .from('db_envs')
+      .select('db_info')
+      .eq('db_id', get_table_metadata_dto.databaseServerID)
+      .single();
+
+    if (metadata_error) {
+      throw metadata_error;
+    }
+
+    // Process the metadata, such that it only returns the table_name and the description of each table for the specified database
+    if (!metadata_data.db_info) {
+      return { data: [] };
+    }
+
+    const database = metadata_data.db_info.databases.find(
+      (db) => db.schema_name === get_table_metadata_dto.database
+    );
+
+    if (!database || !database.tables) {
+      return { data: [] };
+    }
+
+    const processed_data = database.tables.map((table) => {
+      return {
+        table_name: table.table_name,
+        description: table.description
+      };
+    });
+
+    return { data: processed_data };
   }
-  getSavedFieldMetadata(get_field_metadata_dto: Field_Metadata_Dto) {
-    throw new Error('Method not implemented.');
+
+  async getSavedFieldMetadata(get_field_metadata_dto: Field_Metadata_Dto) {
+    // Get the user information
+    const { data: user_data, error: user_error } = await this.supabase
+      .getClient()
+      .auth.getUser(this.supabase.getJwt());
+
+    if (user_error) {
+      throw user_error;
+    }
+
+    // Get the org_id from the database
+    const { data: org_data, error: org_error } = await this.supabase
+      .getClient()
+      .from('db_envs')
+      .select('org_id')
+      .eq('db_id', get_field_metadata_dto.databaseServerID)
+      .single();
+
+    if (org_error) {
+      throw org_error;
+    }
+
+    // Get the metadata
+    const { data: metadata_data, error: metadata_error } = await this.supabase
+      .getClient()
+      .from('db_envs')
+      .select('db_info')
+      .eq('db_id', get_field_metadata_dto.databaseServerID)
+      .single();
+
+    if (metadata_error) {
+      throw metadata_error;
+    }
+
+    // Process the metadata, such that it only returns the column_name and the description of each column for the specified database and table
+    if (!metadata_data.db_info) {
+      return { data: [] };
+    }
+
+    const database = metadata_data.db_info.databases.find(
+      (db) => db.schema_name === get_field_metadata_dto.database
+    );
+
+    if (!database || !database.tables) {
+      return { data: [] };
+    }
+
+    const table = database.tables.find(
+      (table) => table.table_name === get_field_metadata_dto.table
+    );
+
+    if (!table || !table.fields) {
+      return { data: [] };
+    }
+
+    const processed_data = table.fields.map((field) => {
+      return {
+        name: field.column_name,
+        description: field.description
+      };
+    });
+
+    return { data: processed_data };
   }
-  getSavedForeignKeyMetadata(get_fk_metadata_dto: Foreign_Key_Metadata_Dto) {
-    throw new Error('Method not implemented.');
+  async getSavedForeignKeyMetadata(
+    get_fk_metadata_dto: Foreign_Key_Metadata_Dto
+  ) {
+    // Get the user information
+    const { data: user_data, error: user_error } = await this.supabase
+      .getClient()
+      .auth.getUser(this.supabase.getJwt());
+
+    if (user_error) {
+      throw user_error;
+    }
+
+    // Get the org_id from the database
+    const { data: org_data, error: org_error } = await this.supabase
+      .getClient()
+      .from('db_envs')
+      .select('org_id')
+      .eq('db_id', get_fk_metadata_dto.databaseServerID)
+      .single();
+
+    if (org_error) {
+      throw org_error;
+    }
+
+    // Get the metadata
+    const { data: metadata_data, error: metadata_error } = await this.supabase
+      .getClient()
+      .from('db_envs')
+      .select('db_info')
+      .eq('db_id', get_fk_metadata_dto.databaseServerID)
+      .single();
+
+    if (metadata_error) {
+      throw metadata_error;
+    }
+
+    // Process the metadata, such that it only returns foriegn key for the specified database and table
+    if (!metadata_data.db_info) {
+      return { data: [] };
+    }
+
+    const database = metadata_data.db_info.databases.find(
+      (db) => db.schema_name === get_fk_metadata_dto.database
+    );
+
+    if (!database || !database.tables) {
+      return { data: [] };
+    }
+
+    const table = database.tables.find(
+      (table) => table.table_name === get_fk_metadata_dto.table
+    );
+
+    if (!table || !table.foreign_keys) {
+      return { data: [] };
+    }
+
+    const processed_data = table.foreign_keys;
+
+    return { data: processed_data };
   }
-  getSavedServerSummaryMetadata(get_summary_metadata_dto: Server_Summary_Metadata_Dto) {
+  async getSavedServerSummaryMetadata(
+    get_summary_metadata_dto: Server_Summary_Metadata_Dto
+  ) {
     throw new Error('Method not implemented.');
   }
 }
